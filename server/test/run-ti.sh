a8199189a6c813c452e38fb8c73a4cc14bdc7292
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-11-06 11:05:25 +0100

    Update run-ti.sh

--- server/test/run-ti.sh
+++ server/test/run-ti.sh
@@ -2,5 +2,6 @@ cd ../bin
 ./HangmanGame 0.0.0.0 8080 . 1 &
 sleep 30
 curl -X POST http://localhost:8080/api/token -H 'Content-Type: application/json' -H 'Accept: application/json' -d '{"login" : "a", "password" : "b"}'
-sleep 15
-pkill -15 HangmanGame
+sleep 30
+echo "soft stop emitted"
+pkill -INT HangmanGame
