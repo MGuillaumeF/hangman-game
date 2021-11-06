57e4e031d111699d017efb8bd9308e5261c02fc5
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-11-06 06:38:35 +0100

    Correction du call CURL

--- server/test/run-ti.sh
+++ server/test/run-ti.sh
@@ -1,6 +1,6 @@
 cd ../bin
 ./HangmanGame 0.0.0.0 8080 . 1 &
 sleep 30
-curl http://localhost:8080/api/token
+curl -X POST http://localhost:8080/api/token -H 'Content-Type: application/json' -H 'Accept: application/json' -d '{"login" : "a", "password" : "b"}'
 sleep 15
 pkill -15 a.out
