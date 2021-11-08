ba599408de5cfaf77f934bcd4c7f27273eb6b6f5
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-11-09 00:44:20 +0100

    Update HttpServer.cpp

--- server/src/common/HTTP/HttpServer.cpp
+++ server/src/common/HTTP/HttpServer.cpp
@@ -7,12 +7,13 @@
 /**
  * @brief Construct a new Http Server:: Http Server object
  *
+ * @param ioc The IO Context of application
  * @param address The IP address of server
  * @param port The Port of server
  * @param doc_root The path of root directory of static files
  * @param threads The number of thread of server
  */
-HttpServer::HttpServer(const boost::asio::io_context& ioc, const std::string &address, const uint16_t port,
+HttpServer::HttpServer(boost::asio::io_context& ioc, const std::string &address, const uint16_t port,
                        const std::string &doc_root, const uint8_t threads) {
   auto const l_address = boost::asio::ip::make_address(address);
   auto const l_doc_root = std::make_shared<std::string>(doc_root);
