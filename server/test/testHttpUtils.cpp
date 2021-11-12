d2e368be3add3ecbb50082d6da1cbd18c91c96e9
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-11-12 07:54:42 +0100

    Update testHttpUtils.cpp

--- server/test/testHttpUtils.cpp
+++ server/test/testHttpUtils.cpp
@@ -6,7 +6,7 @@
 BOOST_AUTO_TEST_SUITE(testsHttpUtils)
 
 BOOST_AUTO_TEST_CASE(testMimeType) {
-  boost::beast::string_view mimeType = HTTP::Utils::getMimeType("test.html");
+  boost::beast::string_view mimeType = http::Utils::getMimeType("test.html");
   BOOST_CHECK_EQUAL(mimeType, "text/html");
 
   mimeType = http::Utils::getMimeType("./");
