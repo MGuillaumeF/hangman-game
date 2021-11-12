1352bd1ec4b3e17b6390adb5d12399535e302f56
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-11-12 02:04:36 +0100

    Prepare directories

--- server/test/ut/testHttpUtils.cpp
+++ server/test/ut/testHttpUtils.cpp
@@ -1,6 +1,6 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE testHttpUtils
-#include "../src/common/HTTP/Utils.hpp"
+#include "../../src/common/HTTP/Utils.hpp"
 #include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(testsHttpUtils)
