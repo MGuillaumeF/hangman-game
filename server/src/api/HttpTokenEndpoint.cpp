e15e88b90bafc932dde94a50c5fabec453a46c40
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-11-14 15:40:28 +0100

    Update HttpTokenEndpoint.cpp

--- server/src/api/HttpTokenEndpoint.cpp
+++ server/src/api/HttpTokenEndpoint.cpp
@@ -1,6 +1,6 @@
 #include "HttpTokenEndpoint.hpp"
 
-HttpTokenEndpoint:: HttpTokenEndpoint(
+HttpTokenEndpoint::HttpTokenEndpoint(
       const boost::beast::http::request<boost::beast::http::string_body> &req)
       : http::RestrictiveEndpoint(req,
                                 {{boost::beast::http::verb::post, true},
