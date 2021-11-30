4a5bfa3d5ffb6b2604b418e39ac67c3ca376d799
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-11-30 15:48:57 +0100

    Update httpClient.cpp

--- server/test/it/httpClient.cpp
+++ server/test/it/httpClient.cpp
@@ -194,7 +194,7 @@ int32_t main(int argc, char *argv[]) {
 
     requestProperties.methode = boost::beast::http::verb::get;
     requestProperties.body = "";
-    response = sendRequest(requestProperties);
+    /*response = sendRequest(requestProperties);
 
     if (response.result() == boost::beast::http::status::ok) {
       std::cout << "9 : [success]" << std::endl;
@@ -202,7 +202,7 @@ int32_t main(int argc, char *argv[]) {
     } else {
       std::cerr << "9 : [failed]" << std::endl;
       nbFailed++;
-    }
+    }*/
 
     requestProperties.target = "/bin/configuration/mime-types.xml-old";
     response = sendRequest(requestProperties);
