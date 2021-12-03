4ffab3e8c9050bf943ff9b52ebc79f60164c5eee
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-12-03 22:22:06 +0100

    Update testConfigurationServer.cpp

--- server/test/ut/testConfigurationServer.cpp
+++ server/test/ut/testConfigurationServer.cpp
@@ -7,7 +7,7 @@ BOOST_AUTO_TEST_SUITE(testConfigurationServer)
 
 BOOST_AUTO_TEST_CASE(testHttpConfigurationServer) {
   auto config = ConfigurationServer({});
-  BOOST_CHECK(config.getDescription().equals("this is description"));
+  BOOST_CHECK(0 == config.getDescription().compare("this is description"));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
