bba3774412108e48bea68f072a6abb85ede2004c
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-12-04 15:41:04 +0100

    Update testConfigurationServer.cpp

--- server/test/ut/testConfigurationServer.cpp
+++ server/test/ut/testConfigurationServer.cpp
@@ -1,4 +1,4 @@
-// #define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_DYN_LINK
 
 #include "../../src/common/HTTP/Configuration/ConfigurationServer.hpp"
 // #include <boost/test/included/unit_test.hpp>
