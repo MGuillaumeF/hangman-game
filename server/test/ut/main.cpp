56b9932ab11794dee34251cf05a325cda7dfee02
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-12-04 14:54:48 +0100

    Update main.cpp

--- server/test/ut/main.cpp
+++ server/test/ut/main.cpp
@@ -1,9 +1,9 @@
-#define BOOST_TEST_DYN_LINK
+// #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE HangmanGameTest
-#define BOOST_TEST_NO_MAIN
+// #define BOOST_TEST_NO_MAIN
 
 #include <boost/test/unit_test.hpp>
-
+/*
 bool init_function() {
   // before all
   boost::unit_test::framework::master_test_suite().p_name.value = "HangmanGameTestSuite";
@@ -14,3 +14,4 @@ int main(int argc, char* argv[]) {
   int all_test_result = ::boost::unit_test::unit_test_main(&init_function, argc, argv);
   return all_test_result;
 }
+*/
