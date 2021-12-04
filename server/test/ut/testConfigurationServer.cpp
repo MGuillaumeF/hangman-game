6867a18e735c051253c392a78ccfd340dd31331a
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-12-04 22:46:22 +0100

    Update testConfigurationServer.cpp

--- server/test/ut/testConfigurationServer.cpp
+++ server/test/ut/testConfigurationServer.cpp
@@ -1,6 +1,7 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
 #include "../../src/common/HTTP/Configuration/ConfigurationServer.hpp"
-// #include <boost/test/included/unit_test.hpp>
-#include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(testConfigurationServer)
 
