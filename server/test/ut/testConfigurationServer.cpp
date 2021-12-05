27012b5e80f31849f7251e45ee30988ef6f4fb2c
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-12-05 14:06:05 +0100

    Update testConfigurationServer.cpp

--- server/test/ut/testConfigurationServer.cpp
+++ server/test/ut/testConfigurationServer.cpp
@@ -8,10 +8,10 @@ BOOST_AUTO_TEST_SUITE(testConfigurationServer)
 BOOST_AUTO_TEST_CASE(testHttpConfigurationServer) {
   auto config = ConfigurationServer({});
   BOOST_CHECK(0 == config.getDescription().compare("example of server 1 configuration"));
-  BOOST_CHECK(0 == config.getHostname.compare("localhost"));
+  BOOST_CHECK(0 == config.getHostname().compare("localhost"));
   BOOST_CHECK(0 == config.getRoot().compare("."));
   BOOST_CHECK_EQUAL(80, config.getPort());
-  BOOST_CHECK_EQUAL(1, config.getThread());
+  BOOST_CHECK_EQUAL(1, config.getThreads());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
