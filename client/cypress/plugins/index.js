ca8cc68aa2860e916ed1ace070b5f8173485d916
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2021-12-28 21:54:45 +0100

    Update index.js

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -1,6 +1,6 @@
-
+const codeCoverageTask = require('@cypress/code-coverage/task');
 module.exports = (on, config) => {
-  require('@cypress/code-coverage/task')(on, config)
+  codeCoverageTask(on, config)
   // include any other plugin code...
 
   // It's IMPORTANT to return the config object
