79af98f3ea3567784dfb5ccdb4de53c185eb0dd0
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-01-01 09:10:59 +0100

    Add convertor of cppcheck report

--- tools/cppcheck-to-sonarqube/src/main.cpp
+++ tools/cppcheck-to-sonarqube/src/main.cpp
@@ -1,6 +1,7 @@
 #include "report/convertor.hpp"
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/xml_parser.hpp>
+#include <iostream>
 
 /**
  * @brief
@@ -10,9 +11,19 @@
  * @return int The process exit code
  */
 int main(int argc, char *argv[]) {
-  boost::property_tree::ptree cppCheckReport =
-      Convertor::readCppCheckReport(argv[1]);
-  boost::property_tree::ptree sonarqubeReport =
-      Convertor::cppCheckReportToSonarqubeReportTree(cppCheckReport);
-  boost::property_tree::write_json(argv[1], sonarqubeReport);
+  try {
+    if (argc == 3) {
+      boost::property_tree::ptree cppCheckReport =
+          Convertor::readCppCheckReport(argv[1]);
+      boost::property_tree::ptree sonarqubeReport =
+          Convertor::cppCheckReportToSonarqubeReportTree(cppCheckReport);
+      boost::property_tree::write_json(argv[2], sonarqubeReport);
+    } else {
+      std::cerr << "Bad usage of tool : " << std::endl
+                << "  example of expected usage : cppcheck-to-sonarqube "
+                   "cppcheck-report.xml cppcheck-sonarqube-report.json";
+    }
+  } catch (const std::exception &ex) {
+    std::cerr << "Error : " << ex.what() << std::endl;
+  }
 }
--- tools/cppcheck-to-sonarqube/src/report/convertor.cpp
+++ tools/cppcheck-to-sonarqube/src/report/convertor.cpp
@@ -1,15 +1,119 @@
 #include "convertor.hpp"
 #include <boost/property_tree/xml_parser.hpp>
+#include <map>
 
+/**
+ * @brief SonarCloud severity levels of issues
+ *
+ */
+enum SonarCloudSeverity { INFO, MINOR, MAJOR, CRITICAL, BLOCKER };
+
+/**
+ * @brief map to convert cppcheck severity to sonacloud severity
+ *
+ */
+const std::map<std::string, SonarCloudSeverity> cppcheckToSonarCloudSeverity = {
+    {"none", INFO},
+    {"debug", INFO},
+    {"information", INFO},
+    {"style", MINOR},
+    {"warning", MAJOR},
+    {"portability", MAJOR},
+    {"performance", CRITICAL},
+    {"error", BLOCKER}};
+
+/**
+ * @brief map to convert cppcheck severity to sonacloud severity
+ *
+ */
+const std::map<SonarCloudSeverity, std::string> SonarCloudSeverityValue = {
+    {INFO, "INFO"},
+    {MINOR, "MINOR"},
+    {MAJOR, "MAJOR"},
+    {CRITICAL, "CRITICAL"},
+    {BLOCKER, "BLOCKER"}};
+
+/**
+ * @brief Method to read cppcheck file
+ *
+ * @param filename The path of cppcheck report file
+ * @return boost::property_tree::ptree
+ */
 boost::property_tree::ptree
 Convertor::readCppCheckReport(const std::string &filename) {
   boost::property_tree::ptree cppCheckReport;
   boost::property_tree::read_xml(filename, cppCheckReport);
   return cppCheckReport;
 }
+
+/**
+ * @brief
+ *
+ * @param cppCheckTree
+ * @return boost::property_tree::ptree
+ */
 boost::property_tree::ptree Convertor::cppCheckReportToSonarqubeReportTree(
     const boost::property_tree::ptree &cppCheckTree) {
   boost::property_tree::ptree sonarQubeReport;
+
+  const std::string cppckecVerison =
+      cppCheckTree.get<std::string>("results.cppcheck.<xmlattr>.version");
+
+  const boost::property_tree::ptree errors =
+      cppCheckTree.get_child("results.errors");
+
+  boost::property_tree::ptree issues;
+
+  for (const auto &error : errors) {
+    boost::property_tree::ptree issue;
+    issue.put<std::string>("engineId", "cppcheck-" + cppckecVerison);
+    const std::string ruleId = error.second.get<std::string>("<xmlattr>.id");
+    const std::string severity =
+        error.second.get<std::string>("<xmlattr>.severity");
+    issue.put<std::string>("ruleId", ruleId);
+    issue.put<std::string>(
+        "severity",
+        SonarCloudSeverityValue.at(cppcheckToSonarCloudSeverity.at(severity)));
+    issue.put<std::string>("type", "CODE_SMELL");
+    const std::string message = error.second.get<std::string>("<xmlattr>.msg");
+
+    bool first = true;
+    boost::property_tree::ptree secondaryLocations;
+    for (const auto &errorChild : error.second) {
+      if (errorChild.first == "location") {
+        const std::string filePath =
+            errorChild.second.get<std::string>("<xmlattr>.file");
+
+        boost::property_tree::ptree location;
+        location.put<std::string>("message", message);
+        location.put<std::string>("filePath", filePath);
+
+        boost::property_tree::ptree textRange;
+        textRange.put<std::string>(
+            "startLine", errorChild.second.get<std::string>("<xmlattr>.line"));
+        textRange.put<std::string>(
+            "startColumn",
+            errorChild.second.get<std::string>("<xmlattr>.column"));
+        location.add_child("textRange", textRange);
+
+        if (first) {
+          issue.add_child("primaryLocation", location);
+          first = false;
+        } else {
+          secondaryLocations.push_back(
+              std::pair<const std::string, boost::property_tree::ptree>(
+                  "", location));
+        }
+      }
+    }
+    if (secondaryLocations.size() > 0) {
+      issue.add_child("secondaryLocations", secondaryLocations);
+    }
+    issues.push_back(
+        std::pair<const std::string, boost::property_tree::ptree>("", issue));
+  }
+
+  sonarQubeReport.add_child("issues", issues);
 
   return sonarQubeReport;
 }
