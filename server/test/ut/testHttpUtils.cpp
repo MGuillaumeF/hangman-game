86fcf907b1855381d207f8bf8a0ca9b15dc9139a
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-01-05 01:24:06 +0100

    Update testHttpUtils.cpp

--- server/test/ut/testHttpUtils.cpp
+++ server/test/ut/testHttpUtils.cpp
@@ -9,25 +9,23 @@ BOOST_AUTO_TEST_CASE(testMimeType) {
   std::string mimeType = http::Utils::getMimeType("test.html");
   BOOST_CHECK_EQUAL("text/html", mimeType);
 
-/*
   mimeType = http::Utils::getMimeType("./");
-  BOOST_CHECK(mimeType.compare("application/text"));
+  BOOST_CHECK_EQUAL("application/text", mimeType);
 
   mimeType = http::Utils::getMimeType("");
-  BOOST_CHECK(mimeType.compare("application/text"));
+  BOOST_CHECK_EQUAL("application/text", mimeType);
 
   mimeType = http::Utils::getMimeType("file/to/image.jpe");
-  BOOST_CHECK(mimeType.compare("image/jpeg"));
+  BOOST_CHECK_EQUAL("image/jpeg", mimeType);
 
   mimeType = http::Utils::getMimeType("file/to/textFile.txt");
-  BOOST_CHECK(mimeType.compare("text/plain"));
+  BOOST_CHECK_EQUAL("text/plain", mimeType);
 
   mimeType = http::Utils::getMimeType("file/to/image.gif");
-  BOOST_CHECK(mimeType.compare("image/gif"));
+  BOOST_CHECK_EQUAL("image/gif", mimeType);
 
   mimeType = http::Utils::getMimeType("file/to/file/without/extension");
-  BOOST_CHECK(mimeType.compare("application/text"));
-*/
+  BOOST_CHECK_EQUAL("application/text", mimeType);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
