5d1270688ede7813d1544baa7e7cd4a6d491fa71
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-01-07 08:51:51 +0100

    Update testConfigurationServer.cpp (#179)
    * Update testConfigurationServer.cpp
    
    * Update testConfigurationServer.cpp

--- server/test/ut/testConfigurationServer.cpp
+++ server/test/ut/testConfigurationServer.cpp
@@ -2,6 +2,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include "../../src/common/HTTP/Configuration/ConfigurationServer.hpp"
+#include "../../src/common/HTTP/Exception/ConfigurationException.hpp"
 
 BOOST_AUTO_TEST_SUITE(testConfigurationServer)
 
@@ -14,6 +15,17 @@ BOOST_AUTO_TEST_CASE(testHttpConfigurationServer) {
   BOOST_CHECK(0 == config.getRoot().compare("."));
   BOOST_CHECK_EQUAL(8080, config.getPort());
   BOOST_CHECK_EQUAL(1, config.getThreads());
+}
+
+bool configurationErrorMessage(const ConfigurationException& ex)
+{
+    BOOST_CHECK_EQUAL(ex.what(), std::string("HTTP Configuration file is invalid"));
+    return true;
+}
+
+BOOST_AUTO_TEST_CASE(bad_configuration_content)
+{
+    BOOST_CHECK_EXCEPTION(ConfigurationServer("./configuration/mime-types.xml"), ConfigurationException, configurationErrorMessage);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
