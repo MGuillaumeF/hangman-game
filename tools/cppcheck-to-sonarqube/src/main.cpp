63f4951e243989a114ab3fd61c6f0f0c75f622e5
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-01-09 17:49:23 +0100

    Update main.cpp

--- tools/cppcheck-to-sonarqube/src/main.cpp
+++ tools/cppcheck-to-sonarqube/src/main.cpp
@@ -13,29 +13,30 @@
  */
 int main(int argc, char *argv[]) {
   try {
+    boost::property_tree::ptree sonarqubeReport;
     if (argc == 4) {
       if (0 == std::string("cppcheck").compare(argv[1])) {
         boost::property_tree::ptree cppCheckReport =
             Convertor::readCppCheckReport(argv[2]);
-        boost::property_tree::ptree sonarqubeReport =
+        sonarqubeReport =
             Convertor::cppCheckReportToSonarqubeReportTree(cppCheckReport);
-        if (sonarqubeReport.get_child("issues").size() > 0) {
-          boost::property_tree::write_json(argv[3], sonarqubeReport);
-        } else {
-          std::ofstream emptyJsonFile;
-          emptyJsonFile.open(argv[3]);
-          emptyJsonFile << "{\"issues\": []}";
-          emptyJsonFile.close();
-        }
+       
       } else if (0 == std::string("clang-tidy").compare(argv[1])) {
-        boost::property_tree::ptree sonarqubeReport =
+        sonarqubeReport =
             Convertor::clangTidyReportToSonarqubeReportTree(argv[2]);
-        boost::property_tree::write_json(argv[3], sonarqubeReport);
       }
     } else {
       std::cerr << "Bad usage of tool : " << std::endl
                 << "  example of expected usage : cppcheck-to-sonarqube "
                    "cppcheck-report.xml cppcheck-sonarqube-report.json";
+    }
+    if (sonarqubeReport.get_child("issues").size() > 0) {
+      boost::property_tree::write_json(argv[3], sonarqubeReport);
+    } else {
+      std::ofstream emptyJsonFile;
+      emptyJsonFile.open(argv[3]);
+      emptyJsonFile << "{\"issues\": []}";
+      emptyJsonFile.close();
     }
   } catch (const std::exception &ex) {
     std::cerr << "Error : " << ex.what() << std::endl;
