26e6b9ce4e61669fba0b82d16f5badca5835bf71
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-01-13 20:54:32 +0100

    Update main.cpp

--- tools/cppcheck-to-sonarqube/src/main.cpp
+++ tools/cppcheck-to-sonarqube/src/main.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 /**
- * @brief
+ * @brief entry of application
  *
  * @param argc The number of argument of process launch command
  * @param argv The process launch command arguments array
@@ -13,34 +13,42 @@
  */
 int32_t main(int argc, char *argv[]) {
   int32_t exitStatus = EXIT_SUCCESS;
+  const std::string reportType = argv[1];
+  const std::string inputFile = argv[2];
+  const std::string outputFile = argv[3];
   try {
     boost::property_tree::ptree sonarqubeReport;
     if (argc == 4) {
-      if (0 == std::string("cppcheck").compare(argv[1])) {
-        boost::property_tree::ptree cppCheckReport =
-            Convertor::readCppCheckReport(argv[2]);
+      if (reportType == "cppcheck")) {
+        const boost::property_tree::ptree cppCheckReport =
+            Convertor::readCppCheckReport(inputFile);
         sonarqubeReport =
             Convertor::cppCheckReportToSonarqubeReportTree(cppCheckReport);
        
-      } else if (0 == std::string("clang-tidy").compare(argv[1])) {
+      } else if (reportType == "clang-tidy") {
         sonarqubeReport =
-            Convertor::clangTidyReportToSonarqubeReportTree(argv[2]);
+            Convertor::clangTidyReportToSonarqubeReportTree(inputFile);
       }
     } else {
       std::cerr << "Bad usage of tool : " << std::endl
                 << "  example of expected usage : cppcheck-to-sonarqube "
                    "cppcheck-report.xml cppcheck-sonarqube-report.json";
     }
-    if (sonarqubeReport.get_child("issues").size() > 0) {
-      boost::property_tree::write_json(argv[3], sonarqubeReport);
+    // if at least one issue found write property tree json report
+    if (!sonarqubeReport.get_child("issues").empty()) {
+      boost::property_tree::write_json(outputFile, sonarqubeReport);
     } else {
+      // if issues property tree array is empty write empty report 
+      // to have a valid report
       std::ofstream emptyJsonFile;
-      emptyJsonFile.open(argv[3]);
+      emptyJsonFile.open(outputFile);
       emptyJsonFile << "{\"issues\": []}";
       emptyJsonFile.close();
     }
+  // catch all exception to have a managed return with error log
   } catch (const std::exception &ex) {
     std::cerr << "Unexpected Fatal Error : " << ex.what() << std::endl;
+    // change process return value
     exitStatus = EXIT_FAILURE;
   }
   return exitStatus;
