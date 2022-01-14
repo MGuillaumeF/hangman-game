b98d9a32a08f76a86731597ef286587e6daf8af7
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-01-14 22:54:49 +0100

    Update main.cpp (#197)
    * Update main.cpp
    
    * Remove excessive parentheses
    
    * Add PR clang analysis
    
    * Fix syntax
    
    * Remove dependence
    
    * Update default.yml
    
    * Update default.yml
    
    * Update default.yml
    
    * Update default.yml
    
    * Try destructuring
    
    * Update main.cpp
    
    * Update main.cpp
    
    * Try destructuring
    
    * Try destructuring x2
    
    * Update main.cpp
    
    * Update main.cpp
    
    * Destructuring try
    
    * Update main.cpp
    
    * Update main.cpp
    
    * Update main.cpp

--- tools/cppcheck-to-sonarqube/src/main.cpp
+++ tools/cppcheck-to-sonarqube/src/main.cpp
@@ -3,9 +3,10 @@
 #include <boost/property_tree/xml_parser.hpp>
 #include <fstream>
 #include <iostream>
+#include <list>
 
 /**
- * @brief
+ * @brief entry of application
  *
  * @param argc The number of argument of process launch command
  * @param argv The process launch command arguments array
@@ -13,34 +14,50 @@
  */
 int32_t main(int argc, char *argv[]) {
   int32_t exitStatus = EXIT_SUCCESS;
+  const uint8_t ARGUMENTS_SIZE = 4;
+
   try {
     boost::property_tree::ptree sonarqubeReport;
-    if (argc == 4) {
-      if (0 == std::string("cppcheck").compare(argv[1])) {
-        boost::property_tree::ptree cppCheckReport =
-            Convertor::readCppCheckReport(argv[2]);
+    if (argc == ARGUMENTS_SIZE) {
+      std::list<std::string> args(argv + 1, argv + argc);
+
+      const std::string reportType = args.front();
+      args.pop_front();
+      const std::string inputFile = args.front();
+      args.pop_front();
+      const std::string outputFile = args.front();
+      args.pop_front();
+
+      if (reportType == "cppcheck") {
+        const boost::property_tree::ptree cppCheckReport =
+            Convertor::readCppCheckReport(inputFile);
         sonarqubeReport =
             Convertor::cppCheckReportToSonarqubeReportTree(cppCheckReport);
        
-      } else if (0 == std::string("clang-tidy").compare(argv[1])) {
+      } else if (reportType == "clang-tidy") {
         sonarqubeReport =
-            Convertor::clangTidyReportToSonarqubeReportTree(argv[2]);
+            Convertor::clangTidyReportToSonarqubeReportTree(inputFile);
+      }
+      // if at least one issue found write property tree json report
+      if (!sonarqubeReport.get_child("issues").empty()) {
+        boost::property_tree::write_json(outputFile, sonarqubeReport);
+      } else {
+        // if issues property tree array is empty write empty report 
+        // to have a valid report
+        std::ofstream emptyJsonFile;
+        emptyJsonFile.open(outputFile);
+        emptyJsonFile << "{\"issues\": []}";
+        emptyJsonFile.close();
       }
     } else {
       std::cerr << "Bad usage of tool : " << std::endl
                 << "  example of expected usage : cppcheck-to-sonarqube "
                    "cppcheck-report.xml cppcheck-sonarqube-report.json";
     }
-    if (sonarqubeReport.get_child("issues").size() > 0) {
-      boost::property_tree::write_json(argv[3], sonarqubeReport);
-    } else {
-      std::ofstream emptyJsonFile;
-      emptyJsonFile.open(argv[3]);
-      emptyJsonFile << "{\"issues\": []}";
-      emptyJsonFile.close();
-    }
+  // catch all exception to have a managed return with error log
   } catch (const std::exception &ex) {
     std::cerr << "Unexpected Fatal Error : " << ex.what() << std::endl;
+    // change process return value
     exitStatus = EXIT_FAILURE;
   }
   return exitStatus;
