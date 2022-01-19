232f29a6f97d9945e95ab2e53d44102df008aae3
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-01-19 03:02:55 +0100

    Create audit-to-sonar.js

---
+++ client/tools/audit-to-sonar.js
@@ -0,0 +1,7 @@
+(function(){
+  if (process.args.length === 2) {
+      const fs = require('fs').promises;
+  } else {
+      console.error('one argument expected', process.args.length, 'found'; 
+  }
+})();
