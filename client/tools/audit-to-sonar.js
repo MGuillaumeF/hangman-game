370a4e0985e59a6732b10c8b0463a6dd6ee66250
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-01-25 22:57:00 +0100

    Update audit-to-sonar.js

--- client/tools/audit-to-sonar.js
+++ client/tools/audit-to-sonar.js
@@ -1,13 +1,20 @@
 (async function() {
   exitStatus = 0;
+
   // get arguments of process run
   const args = process.argv.slice(2);
+
+  const params = {
+      packageFilePath : path.resolve(process.cwd(), 'package.json'),
+      outputFilePath : path.resolve(process.cwd(), 'dist/reports/audit-report.json')
+      inputFilePath : path.resolve(process.cwd(), inputFile)
+  };
+
   // if args have good size run converting
   if (args.length === 1) {
       const [inputFile] = args;
       const fs = require('fs').promises;
       const path = require('path');
-      const filename = path.resolve(process.cwd(), inputFile);
 
       const npmSeverityToSonar = new Map([
           ["info", 'INFO'],
@@ -19,7 +26,7 @@
 
       let auditJsonString = '';
       try {
-          auditJsonString = await fs.readFile(filename);
+          auditJsonString = await fs.readFile(params.inputFilePath);
           auditJsonString = auditJsonString.toString();
 
       } catch (error) {
@@ -33,7 +40,7 @@
       const engineId = `npm-audit-${audit.auditReportVersion}`;
       for (const [packageName, vulnerability] of Object.entries(audit.vulnerabilities)) {
           if (vulnerability.isDirect) {
-              const packageJsonFile = (await fs.readFile(path.resolve(process.cwd(), 'package.json'))).toString();
+              const packageJsonFile = (await fs.readFile(params.packageFilePath)).toString();
               const packageNameIndex = packageJsonFile.indexOf(packageName);
               const rows = packageJsonFile.slice(0, packageNameIndex).split('\n');
               const startLine = rows.length;
@@ -47,7 +54,7 @@
                   type : 'VULNERABILITY',
                   primaryLocation : {
                       message : `The dependency ${packageName} has vulnerability${vulnerability.fixAvailable ? `, fix available in ${vulnerability.fixAvailable.name} version : ${vulnerability.fixAvailable.version}` : ''}`,
-                      filePath : path.resolve(process.cwd(), 'package.json'),
+                      filePath : params.packageFilePath,
                       textRange : {
                           startLine,
                           startColumn,
@@ -68,7 +75,7 @@
                   const [level, quantity] = entry;
                   return `- ${quantity} ${level}`;
               }).join('\n'),
-              filePath : path.resolve(process.cwd(), 'package.json'),
+              filePath : params.packageFilePath,
               textRange : {
                   startLine : 1,
                   startColumn : 0
@@ -78,7 +85,7 @@
       const output = JSON.stringify({issues}, null, 4);
       console.debug('issues generated', output);
       try {
-          await fs.writeFile(path.resolve(process.cwd(), 'dist/reports/audit-report.json'), output);
+          await fs.writeFile(params.outputFilePath, output);
       } catch (error) {
           console.error('output file write failed', error);
           exitStatus = 2;
