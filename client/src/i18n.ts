e333d3f30d024371f0b830557421f81d2543a302
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-03-02 05:01:06 +0100

    add first usage of i18n

---
+++ client/src/i18n.ts
@@ -0,0 +1,40 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+//import Backend from 'i18next-xhr-backend';
+import en from './locales/en/translation.json';
+import fr from './locales/fr/translation.json';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// not like to use this?
+// have a look at the Quick start guide
+// for passing in lng and translations on init
+const resources = {
+  en: {
+    translation: en,
+  },
+  fr: {
+    translation: fr,
+  },
+};
+i18n
+  // load translation using xhr -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+  // learn more: https://github.com/i18next/i18next-xhr-backend
+  //.use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    resources,
+    fallbackLng: ['en', 'fr'],
+    lng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
+export default i18n;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-perf/jsx-no-new-function-as-prop */
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
@@ -6,12 +7,35 @@ import reportWebVitals from "./reportWebVitals";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import "./index.scss";
+import "./i18n";
+
+import { useTranslation } from "react-i18next";
 
 function App(): JSX.Element {
+  const { t, i18n } = useTranslation();
+  const changeLang = (lang: string) => {
+    i18n.changeLanguage(lang);
+  };
+
+  const changeLangBtnClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ): void => {
+    const currentBtnText = String(
+      event?.currentTarget?.textContent
+    ).toLowerCase();
+    if (["fr", "en"].includes(currentBtnText)) {
+      changeLang(currentBtnText);
+    }
+  };
   return (
     <>
       <Header title="Ceci est mon site" />
       <Main />
+      <form>
+        <h2>{t("forms.sign-in.title")}</h2>
+      </form>
+      <button onClick={changeLangBtnClick}>FR</button>
+      <button onClick={changeLangBtnClick}>EN</button>
     </>
   );
 }
