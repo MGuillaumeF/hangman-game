76dc434a9492432c09be7d417be973d3cccf3abe
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-03-04 01:20:26 +0100

    Update loading.specs.ts

--- client/cypress/integration/loading.specs.ts
+++ client/cypress/integration/loading.specs.ts
@@ -12,5 +12,19 @@ describe('Load presentation page', () => {
     cy.contains("Connexion");
     cy.get("#lang-en").click();
     cy.contains("Login");
+
+    cy.document().then($document => {
+      cy.get('body').then(($div) => {
+        function words(){
+          var image = $document.createElement("div");
+          image.textContent = "Hangman Game v0.1.0";
+          image.backgroundColor = "#000";
+          image.height = "100vh";
+          image.width = "100%";
+          return image
+        }
+        $div.append(words);
+      });
+    });
   });
 });
