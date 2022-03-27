0213f56a6f36c571a71d214ab3c6b88f2fdd295c
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-03-27 03:34:51 +0200

    Hotfix/clean clang tidy (#324)
    * Update Server.hpp
    
    * Update Server.cpp
    
    * Update Server.hpp
    
    * Update Server.cpp
    
    * [AUTO] prettier format write

--- server/src/common/HTTP/Server.hpp
+++ server/src/common/HTTP/Server.hpp
@@ -16,8 +16,8 @@ public:
    * @param port The Port of server
    * @param threads The number of thread of server
    */
-  Server(const std::string &address, const uint16_t port,
-         const uint8_t threads);
+  Server(const std::string &address, const uint16_t &port,
+         const uint8_t &threads);
 };
 } // namespace http
 
