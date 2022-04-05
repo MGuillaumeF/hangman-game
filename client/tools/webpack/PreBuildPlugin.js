aff38314334b629cbc1b63360d5762a677bcc849
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-05 18:12:31 +0000

    [AUTO] prettier format write

--- client/tools/webpack/PreBuildPlugin.js
+++ client/tools/webpack/PreBuildPlugin.js
@@ -20,68 +20,62 @@ class PreBuildPlugin {
       }
     );
   }
-  initGeneratedDirectory () {
+  initGeneratedDirectory() {
     if (!fs.existsSync(path.resolve(__dirname, "../../src/generated"))) {
-          fs.mkdirSync(path.resolve(__dirname, "../../src/generated"));
-        }
+      fs.mkdirSync(path.resolve(__dirname, "../../src/generated"));
+    }
   }
   copyApiDoc() {
     fs.copyFileSync(
-          path.resolve(__dirname, "../../../server/docs/api_doc.json"),
-          path.resolve(__dirname, "../../src/generated/.api_doc.json")
-        );
+      path.resolve(__dirname, "../../../server/docs/api_doc.json"),
+      path.resolve(__dirname, "../../src/generated/.api_doc.json")
+    );
   }
   generateApiParameters() {
     // start reporting generation
-        logger.info("APPLY", "PreBuildPlugin", "RUN");
-        console.log(...Object.keys(stats));
-       
-        const apiDocs = fs
-          .readFileSync(
-            path.resolve(__dirname, "../../../server/docs/api_doc.json")
-          )
-          .toString();
-        const parameters = {};
-        const apiDocsJson = JSON.parse(apiDocs);
-        for (const [endpointName, endpointConfiguration] of Object.entries(
-          apiDocsJson.paths
-        )) {
-          const namesParts = endpointName
-            .split(/[\/\-]/g)
-            .filter((value) => value !== "");
-          console.log("parts", namesParts);
-          const name = [
-            namesParts.shift(),
-            ...namesParts.map((value) =>
-              [value[0].toUpperCase(), ...value.slice(1)].join("")
-            )
-          ].join("");
-          parameters[name] = {
-            path: `${apiDocsJson.basePath}${endpointName}`
+    logger.info("APPLY", "PreBuildPlugin", "RUN");
+    console.log(...Object.keys(stats));
+
+    const apiDocs = fs
+      .readFileSync(
+        path.resolve(__dirname, "../../../server/docs/api_doc.json")
+      )
+      .toString();
+    const parameters = {};
+    const apiDocsJson = JSON.parse(apiDocs);
+    for (const [endpointName, endpointConfiguration] of Object.entries(
+      apiDocsJson.paths
+    )) {
+      const namesParts = endpointName
+        .split(/[\/\-]/g)
+        .filter((value) => value !== "");
+      console.log("parts", namesParts);
+      const name = [
+        namesParts.shift(),
+        ...namesParts.map((value) =>
+          [value[0].toUpperCase(), ...value.slice(1)].join("")
+        )
+      ].join("");
+      parameters[name] = {
+        path: `${apiDocsJson.basePath}${endpointName}`
+      };
+      for (const [methodName, methodConfiguration] of Object.entries(
+        endpointConfiguration
+      )) {
+        parameters[name][methodName] = {};
+        for (const parameter of methodConfiguration.parameters) {
+          parameters[name][methodName][parameter.name] = {
+            name: parameter.name,
+            required: parameter.required || false
           };
-          for (const [methodName, methodConfiguration] of Object.entries(
-            endpointConfiguration
-          )) {
-            parameters[name][methodName] = {};
-            for (const parameter of methodConfiguration.parameters) {
-              parameters[name][methodName][parameter.name] = {
-                name: parameter.name,
-                required: parameter.required || false
-              };
-            }
-          }
         }
-        console.info(
-          "output parameters docs",
-          JSON.stringify(parameters, null, 4)
-        );
-        fs.writeFileSync(
-          path.resolve(
-            __dirname,
-            "../../src/generated/.api_doc_parameters.json"
-          ),
-          JSON.stringify(parameters, null, 4)
-        );
+      }
+    }
+    console.info("output parameters docs", JSON.stringify(parameters, null, 4));
+    fs.writeFileSync(
+      path.resolve(__dirname, "../../src/generated/.api_doc_parameters.json"),
+      JSON.stringify(parameters, null, 4)
+    );
   }
 }
 
