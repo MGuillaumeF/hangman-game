925757ce671928fb5cf43109729740d14ce4dc95
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-06 00:29:29 +0200

    Update PreBuildPlugin.js (#358)
    * Update PreBuildPlugin.js
    
    * [AUTO] prettier format write
    
    * Update PreBuildPlugin.js
    
    * [AUTO] prettier format write
    
    * Update PreBuildPlugin.js
    
    * Update PreBuildPlugin.js
    
    * [AUTO] prettier format write
    
    * Update PreBuildPlugin.js
    
    * [AUTO] prettier format write
    
    * Update PreBuildPlugin.js
    
    * [AUTO] prettier format write
    
    * Update PreBuildPlugin.js

--- client/tools/webpack/PreBuildPlugin.js
+++ client/tools/webpack/PreBuildPlugin.js
@@ -5,73 +5,129 @@ const path = require("path");
 const Logger = require("./Logger");
 const logger = Logger.getInstance();
 logger.setLevel("WARN");
-logger.setLocation("pioupiou.log");
+logger.setLocation("pre-build-plugin.log");
 
 class PreBuildPlugin {
   apply(compiler) {
+    compiler.hooks.environment.tap("PreBuildPlugin", () => {
+      this.initGeneratedDirectory();
+      this.copyApiDoc();
+      this.generateApiParameters();
+    });
     compiler.hooks.beforeCompile.tap(
       "PreBuildPlugin",
       (
         stats /* stats is passed as an argument when done hook is tapped.  */
       ) => {
-        // start reporting generation
-        logger.info("APPLY", "PreBuildPlugin", "RUN");
-        console.log(...Object.keys(stats));
-        if (!fs.existsSync(path.resolve(__dirname, "../../src/generated"))) {
-          fs.mkdirSync(path.resolve(__dirname, "../../src/generated"));
-        }
-        fs.copyFileSync(
-          path.resolve(__dirname, "../../../server/docs/api_doc.json"),
-          path.resolve(__dirname, "../../src/generated/.api_doc.json")
-        );
-        const apiDocs = fs
-          .readFileSync(
-            path.resolve(__dirname, "../../../server/docs/api_doc.json")
-          )
-          .toString();
-        const parameters = {};
-        const apiDocsJson = JSON.parse(apiDocs);
-        for (const [endpointName, endpointConfiguration] of Object.entries(
-          apiDocsJson.paths
-        )) {
-          const namesParts = endpointName
-            .split(/[\/\-]/g)
-            .filter((value) => value !== "");
-          console.log("parts", namesParts);
-          const name = [
-            namesParts.shift(),
-            ...namesParts.map((value) =>
-              [value[0].toUpperCase(), ...value.slice(1)].join("")
-            )
-          ].join("");
-          parameters[name] = {
-            path: `${apiDocsJson.basePath}${endpointName}`
+        console.log("BEFORE COMPILE STATS DATAS", ...Object.keys(stats));
+        this.initGeneratedDirectory();
+        this.copyApiDoc();
+        this.generateApiParameters();
+      }
+    );
+  }
+  initGeneratedDirectory() {
+    if (!fs.existsSync(path.resolve(__dirname, "../../src/generated"))) {
+      fs.mkdirSync(path.resolve(__dirname, "../../src/generated"));
+    }
+  }
+  copyApiDoc() {
+    let mustBeCopied = false;
+    if (
+      !fs.existsSync(
+        path.resolve(__dirname, "../../src/generated/.api_doc.json")
+      )
+    ) {
+      mustBeCopied = true;
+    } else {
+      const externalInputDoc = fs.readFileSync(
+        path.resolve(__dirname, "../../../server/docs/api_doc.json")
+      );
+      const currentInputDoc = fs.readFileSync(
+        path.resolve(__dirname, "../../src/generated/.api_doc.json")
+      );
+      if (!externalInputDoc.equals(currentInputDoc)) {
+        mustBeCopied = true;
+      }
+    }
+    if (mustBeCopied) {
+      console.log("COPY DOC FILE");
+      fs.copyFileSync(
+        path.resolve(__dirname, "../../../server/docs/api_doc.json"),
+        path.resolve(__dirname, "../../src/generated/.api_doc.json")
+      );
+    }
+  }
+  generateApiParameters() {
+    // start reporting generation
+    logger.info("GENERATION", "PreBuildPlugin", "RUN");
+
+    const apiDocs = fs
+      .readFileSync(
+        path.resolve(__dirname, "../../../server/docs/api_doc.json")
+      )
+      .toString();
+    const parameters = {};
+    const apiDocsJson = JSON.parse(apiDocs);
+    for (const [endpointName, endpointConfiguration] of Object.entries(
+      apiDocsJson.paths
+    )) {
+      const namesParts = endpointName
+        .split(/[\/\-]/g)
+        .filter((value) => value !== "");
+      console.log("parts", namesParts);
+      const name = [
+        namesParts.shift(),
+        ...namesParts.map((value) =>
+          [value[0].toUpperCase(), ...value.slice(1)].join("")
+        )
+      ].join("");
+      parameters[name] = {
+        path: `${apiDocsJson.basePath}${endpointName}`
+      };
+      for (const [methodName, methodConfiguration] of Object.entries(
+        endpointConfiguration
+      )) {
+        parameters[name][methodName] = {};
+        for (const parameter of methodConfiguration.parameters) {
+          parameters[name][methodName][parameter.name] = {
+            name: parameter.name,
+            required: parameter.required || false
           };
-          for (const [methodName, methodConfiguration] of Object.entries(
-            endpointConfiguration
-          )) {
-            parameters[name][methodName] = {};
-            for (const parameter of methodConfiguration.parameters) {
-              parameters[name][methodName][parameter.name] = {
-                name: parameter.name,
-                required: parameter.required || false
-              };
-            }
-          }
         }
-        console.info(
-          "output parameters docs",
-          JSON.stringify(parameters, null, 4)
-        );
-        fs.writeFileSync(
+      }
+    }
+    let mustBeWrited = false;
+    const dataToWrite = JSON.stringify(parameters, null, 4);
+    if (
+      !fs.existsSync(
+        path.resolve(
           path.resolve(
             __dirname,
             "../../src/generated/.api_doc_parameters.json"
-          ),
-          JSON.stringify(parameters, null, 4)
-        );
+          )
+        )
+      )
+    ) {
+      mustBeWrited = true;
+    } else {
+      const currentInputDoc = fs.readFileSync(
+        path.resolve(__dirname, "../../src/generated/.api_doc_parameters.json")
+      );
+      if (currentInputDoc.toString() !== dataToWrite) {
+        mustBeWrited = true;
       }
-    );
+    }
+    if (mustBeWrited) {
+      console.info(
+        "output parameters docs",
+        JSON.stringify(parameters, null, 4)
+      );
+      fs.writeFileSync(
+        path.resolve(__dirname, "../../src/generated/.api_doc_parameters.json"),
+        dataToWrite
+      );
+    }
   }
 }
 
