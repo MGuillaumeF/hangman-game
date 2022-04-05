b4f873112f6b21e26b7016324285c8b472972425
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-05 02:24:37 +0200

    Add pre build processed file (#355)
    * Update PreBuildPlugin.js
    
    * [AUTO] prettier format write
    
    * Update PreBuildPlugin.js
    
    * Update PreBuildPlugin.js
    
    * Update PreBuildPlugin.js
    
    * Update PreBuildPlugin.js
    
    * [AUTO] prettier format write
    
    * Update PreBuildPlugin.js
    
    * [AUTO] prettier format write
    
    * Update PreBuildPlugin.js
    
    * Update PreBuildPlugin.js
    
    * [AUTO] prettier format write
    
    * Update PreBuildPlugin.js
    
    * [AUTO] prettier format write
    
    * Update PreBuildPlugin.js
    
    * Update SignIn.tsx
    
    * [AUTO] prettier format write
    
    * Update SignIn.tsx

--- client/src/components/Forms/SignIn/SignIn.tsx
+++ client/src/components/Forms/SignIn/SignIn.tsx
@@ -1,20 +1,15 @@
 import React, { FormEvent, HTMLProps, useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
-import api from "../../../generated/.api_doc.json";
-import { isValidField } from "../../../model/fields";
+import api2 from "../../../generated/.api_doc_parameters.json";
 import BasicInput from "../../BasicInput/BasicInput";
 import BasicForm from "../BasicForm.scss";
 
 const formStyle: React.CSSProperties = { marginTop: "1.5em" };
 
 const FIELDS = {
-  login: api.paths["/user/sign-in"].post.parameters[0],
-  password: api.paths["/user/sign-in"].post.parameters[1]
+  login: api2.userSignIn.post.login,
+  password: api2.userSignIn.post.password
 };
-
-for (const [fieldName, fieldValues] of Object.entries(FIELDS)) {
-  isValidField(fieldName, fieldValues);
-}
 
 type Props = {
   fieldsetProperties?: HTMLProps<HTMLFieldSetElement>;
@@ -23,8 +18,8 @@ type Props = {
   title?: string;
 };
 
-const loginInputProperties = { required: FIELDS.login?.required };
-const passwordInputProperties = { required: FIELDS.password?.required };
+const loginInputProperties = { required: FIELDS.login.required };
+const passwordInputProperties = { required: FIELDS.password.required };
 
 /**
  * onSubmit function
@@ -36,14 +31,11 @@ const onSubmitRequest = async (
   resultUpdater: (success: boolean, error?: string) => void
 ): Promise<void> => {
   event.preventDefault();
-  const fields = [FIELDS.login?.name, FIELDS.password?.name];
+  const fields = [FIELDS.login.name, FIELDS.password.name];
   const formElements = event.currentTarget.elements;
 
   const formData = new Map<string, string>(
-    fields.map((fieldName: string | undefined) => {
-      if (fieldName === undefined) {
-        throw Error("Documentation Error");
-      }
+    fields.map((fieldName: string) => {
       const fieldInput = formElements.namedItem(fieldName);
       if (!fieldInput || !(fieldInput instanceof HTMLInputElement)) {
         throw Error("Invalid form usage");
@@ -106,45 +98,40 @@ function SignIn({ fieldsetProperties, formProperties, id, title }: Props) {
 
   return (
     <>
-      {isValidField("login", FIELDS.login) &&
-      isValidField("password", FIELDS.password) ? (
-        <>
-          <form
-            style={formStyle}
-            method="POST"
-            action={`/api/${api.paths["/user/sign-in"]}`}
-            id={id}
-            onSubmit={onSubmit}
-            className={BasicForm.BasicForm}
-            {...formProperties}
-          >
-            <fieldset {...fieldsetProperties}>
-              <legend>{t(title || "FORMS.SIGN_IN.TITLE")}</legend>
-              <BasicInput
-                type="text"
-                name={FIELDS.login.name}
-                id="FORMS.SIGN_IN.FIELDS.IDENTIFIER"
-                inputProperties={loginInputProperties}
-                label={t("FORMS.SIGN_IN.FIELDS.IDENTIFIER.LABEL")}
-              />
+      <form
+        style={formStyle}
+        method="POST"
+        action={`/api${api2.userSignIn.path}`}
+        id={id}
+        onSubmit={onSubmit}
+        className={BasicForm.BasicForm}
+        {...formProperties}
+      >
+        <fieldset {...fieldsetProperties}>
+          <legend>{t(title || "FORMS.SIGN_IN.TITLE")}</legend>
+          <BasicInput
+            type="text"
+            name={FIELDS.login.name}
+            id="FORMS.SIGN_IN.FIELDS.IDENTIFIER"
+            inputProperties={loginInputProperties}
+            label={t("FORMS.SIGN_IN.FIELDS.IDENTIFIER.LABEL")}
+          />
 
-              <BasicInput
-                type="password"
-                name={FIELDS.password.name}
-                id="FORMS.SIGN_IN.FIELDS.PASSWORD"
-                inputProperties={passwordInputProperties}
-                label={t("FORMS.SIGN_IN.FIELDS.PASSWORD.LABEL")}
-              />
-              <input
-                disabled={pendingState}
-                type="submit"
-                value={String(t("FORMS.SIGN_IN.FIELDS.SUBMIT.LABEL"))}
-              />
-            </fieldset>
-          </form>
-          {pendingState ? <div>loading</div> : null}
-        </>
-      ) : null}
+          <BasicInput
+            type="password"
+            name={FIELDS.password.name}
+            id="FORMS.SIGN_IN.FIELDS.PASSWORD"
+            inputProperties={passwordInputProperties}
+            label={t("FORMS.SIGN_IN.FIELDS.PASSWORD.LABEL")}
+          />
+          <input
+            disabled={pendingState}
+            type="submit"
+            value={String(t("FORMS.SIGN_IN.FIELDS.SUBMIT.LABEL"))}
+          />
+        </fieldset>
+      </form>
+      {pendingState ? <div>loading</div> : null}
     </>
   );
 }
--- client/tools/webpack/PreBuildPlugin.js
+++ client/tools/webpack/PreBuildPlugin.js
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const Logger = require("./Logger");
 const fs = require("fs");
 const path = require("path");
+
+const Logger = require("./Logger");
 const logger = Logger.getInstance();
 logger.setLevel("WARN");
 logger.setLocation("pioupiou.log");
+
 class PreBuildPlugin {
   apply(compiler) {
     compiler.hooks.beforeCompile.tap(
@@ -21,6 +23,52 @@ class PreBuildPlugin {
         fs.copyFileSync(
           path.resolve(__dirname, "../../../server/docs/api_doc.json"),
           path.resolve(__dirname, "../../src/generated/.api_doc.json")
+        );
+        const apiDocs = fs
+          .readFileSync(
+            path.resolve(__dirname, "../../../server/docs/api_doc.json")
+          )
+          .toString();
+        const parameters = {};
+        const apiDocsJson = JSON.parse(apiDocs);
+        for (const [endpointName, endpointConfiguration] of Object.entries(
+          apiDocsJson.paths
+        )) {
+          const namesParts = endpointName
+            .split(/[\/\-]/g)
+            .filter((value) => value !== "");
+          console.log("parts", namesParts);
+          const name = [
+            namesParts.shift(),
+            ...namesParts.map((value) =>
+              [value[0].toUpperCase(), ...value.slice(1)].join("")
+            )
+          ].join("");
+          parameters[name] = {
+            path: `${apiDocsJson.basePath}${endpointName}`
+          };
+          for (const [methodName, methodConfiguration] of Object.entries(
+            endpointConfiguration
+          )) {
+            parameters[name][methodName] = {};
+            for (const parameter of methodConfiguration.parameters) {
+              parameters[name][methodName][parameter.name] = {
+                name: parameter.name,
+                required: parameter.required || false
+              };
+            }
+          }
+        }
+        console.info(
+          "output parameters docs",
+          JSON.stringify(parameters, null, 4)
+        );
+        fs.writeFileSync(
+          path.resolve(
+            __dirname,
+            "../../src/generated/.api_doc_parameters.json"
+          ),
+          JSON.stringify(parameters, null, 4)
         );
       }
     );
