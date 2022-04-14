f3b3415f628d97ae1f5f4ae9122d0077fb478925
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-14 16:57:36 +0000

    [AUTO] prettier format write

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -293,13 +293,13 @@ module.exports = (env, args) => {
         //new PostBuildPlugin()
       ),
       new CompressionPlugin({
-      compressionOptions: {
-        numiterations: 15,
-      },
-      algorithm(input, compressionOptions, callback) {
-        return zopfli.gzip(input, compressionOptions, callback);
-      },
-    })
+        compressionOptions: {
+          numiterations: 15
+        },
+        algorithm(input, compressionOptions, callback) {
+          return zopfli.gzip(input, compressionOptions, callback);
+        }
+      })
     );
   }
   return config;
