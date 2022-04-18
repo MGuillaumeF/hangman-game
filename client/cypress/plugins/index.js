fd1d69b53f3bb59c23e7fb36e5eaa26c28373b76
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-18 20:23:55 +0000

    [AUTO] prettier format write

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -5,14 +5,14 @@ module.exports = (on, config) => {
   cypressMochawesomeReporter(on);
   // include any other plugin code...
 
-    // https://docs.cypress.io/api/plugins/after-run-api
-    on('after:run', (results) => {
-		// /!\ don't forget to return the Promise /!\
-        return require('cypress-sonarqube-reporter/mergeReports')(results, {
-			// see "Merge Plugin Options" section for all available options
-			mergeFileName: 'sonar-report-all.xml'
-		});
+  // https://docs.cypress.io/api/plugins/after-run-api
+  on("after:run", (results) => {
+    // /!\ don't forget to return the Promise /!\
+    return require("cypress-sonarqube-reporter/mergeReports")(results, {
+      // see "Merge Plugin Options" section for all available options
+      mergeFileName: "sonar-report-all.xml"
     });
+  });
 
   // It's IMPORTANT to return the config object
   // with any changed environment variables
