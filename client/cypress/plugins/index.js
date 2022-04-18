fd7f6a1a423a59fa1614989b9ef48c6c74875b0b
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-19 01:31:24 +0200

    Feature/add mocha reporter (#398)
    * Update package.json
    
    * Update cypress.json
    
    * Update index.js
    
    * Update index.js
    
    * [AUTO] prettier format write
    
    * Update package.json
    
    * [AUTO] prettier format write
    
    * Update loading.specs.ts
    
    * Update index.js
    
    * Update cypress.json
    
    * Update package.json
    
    * [AUTO] prettier format write
    
    * Update sonar-project.properties
    
    * Update cypress.json
    
    * Update default.yml
    
    * Update default.yml

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -1,7 +1,18 @@
 const registerCodeCoverageTasks = require("@cypress/code-coverage/task");
+const cypressMochawesomeReporter = require("cypress-mochawesome-reporter/plugin");
 module.exports = (on, config) => {
   registerCodeCoverageTasks(on, config);
+  cypressMochawesomeReporter(on);
   // include any other plugin code...
+
+  // https://docs.cypress.io/api/plugins/after-run-api
+  on("after:run", (results) => {
+    // /!\ don't forget to return the Promise /!\
+    return require("cypress-sonarqube-reporter/mergeReports")(results, {
+      // see "Merge Plugin Options" section for all available options
+      mergeFileName: "sonar-report-all.xml"
+    });
+  });
 
   // It's IMPORTANT to return the config object
   // with any changed environment variables
