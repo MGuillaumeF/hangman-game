9ca8ca2f3b1694b4cc70d351feb794204579ae11
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-18 21:55:52 +0200

    Update index.js

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -5,6 +5,15 @@ module.exports = (on, config) => {
   cypressMochawesomeReporter(on);
   // include any other plugin code...
 
+    // https://docs.cypress.io/api/plugins/after-run-api
+    on('after:run', (results) => {
+		// /!\ don't forget to return the Promise /!\
+        return require('cypress-sonarqube-reporter/mergeReports')(results, {
+			// see "Merge Plugin Options" section for all available options
+			mergeFileName: 'sonar-report-all.xml'
+		});
+    });
+
   // It's IMPORTANT to return the config object
   // with any changed environment variables
   return config;
