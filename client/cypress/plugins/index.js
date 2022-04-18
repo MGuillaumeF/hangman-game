d9c639fc3cd3410a6601f901cd53b1850e84c799
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-18 15:23:39 +0200

    Update index.js

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -1,6 +1,8 @@
 const registerCodeCoverageTasks = require("@cypress/code-coverage/task");
+const cypressMochawesomeReporter = require('cypress-mochawesome-reporter/plugin');
 module.exports = (on, config) => {
   registerCodeCoverageTasks(on, config);
+  cypressMochawesomeReporter(on);
   // include any other plugin code...
 
   // It's IMPORTANT to return the config object
