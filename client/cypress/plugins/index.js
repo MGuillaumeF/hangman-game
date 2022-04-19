8af25b53db2c2ea6d374b63e7b73dfaa8cf5e30b
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-19 19:49:00 +0200

    Update index.js

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -1,20 +1,11 @@
 const registerCodeCoverageTasks = require("@cypress/code-coverage/task");
-const {
-  beforeRunHook,
-  afterRunHook
-} = require("cypress-mochawesome-reporter/lib");
 
 module.exports = (on, config) => {
   registerCodeCoverageTasks(on, config);
   // include any other plugin code...
 
-  on("before:run", async (details) => {
-    await beforeRunHook(details);
-  });
-
   // https://docs.cypress.io/api/plugins/after-run-api
   on("after:run", async (results) => {
-    await afterRunHook();
     // /!\ don't forget to return the Promise /!\
     await require("cypress-sonarqube-reporter/mergeReports")(results, {
       // see "Merge Plugin Options" section for all available options
