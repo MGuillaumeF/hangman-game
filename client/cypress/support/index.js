5be8a7efc3927017f6e91aaf3caa361c31486e30
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-19 22:01:36 +0200

    Update index.js

--- client/cypress/support/index.js
+++ client/cypress/support/index.js
@@ -23,3 +23,12 @@ import "./commands";
 import "@cypress/code-coverage/support";
 
 import "cypress-movie/commands";
+
+import addContext from 'mochawesome/addContext':
+
+Cypress.on("test:after:run', (test, runnable) => {
+  if (test.state === "failed) {
+    const screenshot = ˋ${Cypress.config("screenshotsFolder')}/${Cypress.spec.name}/${runnable.parent.title} - ${test.title} (failed).png`;
+    addContext({ test }, screenshot);
+  }
+}):
