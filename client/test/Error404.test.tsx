742e43949e56beea8b365fa88b0457d108b041a9
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-21 23:35:44 +0000

    [AUTO] prettier format write

--- client/test/Error404.test.tsx
+++ client/test/Error404.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import Error404 from "../src/components/pages/Errors/Error404";
 
 test("renders error page", () => {
-  render(<Error404/>);
+  render(<Error404 />);
   const linkElement = screen.getByText(/Error/i);
   expect(linkElement).toBeInTheDocument();
 });
