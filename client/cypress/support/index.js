98ccebe8ec6e3b97cac87909203c320c771fb3e9
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-21 00:14:08 +0000

    [AUTO] prettier format write

--- client/cypress/support/index.js
+++ client/cypress/support/index.js
@@ -30,18 +30,19 @@ import fs from "fs";
 
 Cypress.on("test:after:run", (test, runnable) => {
   if (test.state === "failed") {
-  // `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
-  const screenshot = `${Cypress.config("screenshotsFolder")}/${
-    Cypress.spec.name
-  }/Errors pages -- Not Found (failed).png`;
-  // `${Cypress.config("screenshotsFolder")}/${
-  //      Cypress.spec.name
-  //    }/${runnable.parent.title} -- ${test.title} (failed).png`;
-  fs.writeFileSync(Cypress.config("screenshotsFolder") + '/assets/dump.txt',
-    `add screenshot in report failed case, ${screenshot}, file exist ? ${fs.existsSync(
-      screenshot
-    )}`
-  );
+    // `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
+    const screenshot = `${Cypress.config("screenshotsFolder")}/${
+      Cypress.spec.name
+    }/Errors pages -- Not Found (failed).png`;
+    // `${Cypress.config("screenshotsFolder")}/${
+    //      Cypress.spec.name
+    //    }/${runnable.parent.title} -- ${test.title} (failed).png`;
+    fs.writeFileSync(
+      Cypress.config("screenshotsFolder") + "/assets/dump.txt",
+      `add screenshot in report failed case, ${screenshot}, file exist ? ${fs.existsSync(
+        screenshot
+      )}`
+    );
     addContext({ test }, screenshot);
   }
 });
