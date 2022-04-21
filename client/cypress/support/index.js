546cb366196ac3c653ccabed13e908cf438065aa
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-21 02:07:01 +0200

    Update index.js

--- client/cypress/support/index.js
+++ client/cypress/support/index.js
@@ -29,7 +29,7 @@ import addContext from "mochawesome/addContext";
 import fs from "fs";
 
 Cypress.on("test:after:run", (test, runnable) => {
-  //if (test.state === "failed") {
+  if (test.state === "failed") {
   // `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
   const screenshot = `${Cypress.config("screenshotsFolder")}/${
     Cypress.spec.name
@@ -37,17 +37,11 @@ Cypress.on("test:after:run", (test, runnable) => {
   // `${Cypress.config("screenshotsFolder")}/${
   //      Cypress.spec.name
   //    }/${runnable.parent.title} -- ${test.title} (failed).png`;
-  console.info(
+  fs.writeFileSync(Cypress.config("screenshotsFolder") + '/assets/dump.txt',
     `add screenshot in report failed case, ${screenshot}, file exist ? ${fs.existsSync(
       screenshot
     )}`
   );
-  throw Error(
-    `add screenshot in report failed case, ${screenshot}, file exist ? ${fs.existsSync(
-      screenshot
-    )}`
-  );
-
-  addContext({ test }, screenshot);
-  // }
+    addContext({ test }, screenshot);
+  }
 });
