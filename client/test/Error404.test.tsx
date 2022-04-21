f23d91b00762733dcd851245cde2f46f1465865e
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-22 01:28:32 +0200

    Update and rename App.test.tsx to Error404.test.tsx

---
+++ client/test/Error404.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Error404 from "../src/components/pages/Errors/Error404";
+
+test("renders error page", () => {
+  render(<Error404/>);
+  const linkElement = screen.getByText(/Error/i);
+  expect(linkElement).toBeInTheDocument();
+});
