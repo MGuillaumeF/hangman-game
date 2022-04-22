33e4a50804038c6451f03301e3b01d2c4cd37293
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-22 19:32:21 +0200

    Test change name with regex

--- client/cypress/support/index.js
+++ client/cypress/support/index.js
@@ -29,9 +29,8 @@ import addContext from "mochawesome/addContext";
 Cypress.on("test:after:run", (test, runnable) => {
   if (test.state === "failed") {
     // `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
-    const screenshot = `${Cypress.config("screenshotsFolder")}/${
-      Cypress.spec.name
-    }/Errors pages -- Not Found (failed).png`;
+    const screenshot = `assets/${Cypress.spec.name}/${`${runnable.parent.title} -- ${test.title} (failed).png`.replace(/^(.*)\s\[@spec\s\{.*\}\]\s(--\s.*\s\(failed\)\.png)/, "$1 $2");
+}`;
     // `${Cypress.config("screenshotsFolder")}/${
     //      Cypress.spec.name
     //    }/${runnable.parent.title} -- ${test.title} (failed).png`;
