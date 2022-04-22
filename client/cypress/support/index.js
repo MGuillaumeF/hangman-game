636fe9e33accd9c3560350adc1667bd5db60fa5c
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-22 17:55:04 +0000

    [AUTO] prettier format write

--- client/cypress/support/index.js
+++ client/cypress/support/index.js
@@ -29,8 +29,12 @@ import addContext from "mochawesome/addContext";
 Cypress.on("test:after:run", (test, runnable) => {
   if (test.state === "failed") {
     // `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
-    const screenshot = `assets/${Cypress.spec.name}/${`${runnable.parent.title} -- ${test.title} (failed).png`.replace(/^(.*)\s\[@spec\s\{.*\}\]\s(--\s.*\s\(failed\)\.png)$/, "$1 $2")
-}`;
+    const screenshot = `assets/${
+      Cypress.spec.name
+    }/${`${runnable.parent.title} -- ${test.title} (failed).png`.replace(
+      /^(.*)\s\[@spec\s\{.*\}\]\s(--\s.*\s\(failed\)\.png)$/,
+      "$1 $2"
+    )}`;
     // `${Cypress.config("screenshotsFolder")}/${
     //      Cypress.spec.name
     //    }/${runnable.parent.title} -- ${test.title} (failed).png`;
