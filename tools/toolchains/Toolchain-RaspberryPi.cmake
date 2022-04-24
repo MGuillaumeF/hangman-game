6c64cdb39804226a28bad9f2f9ab953bd5384c64
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-24 04:03:32 +0200

    Update Toolchain-RaspberryPi.cmake

--- tools/toolchains/Toolchain-RaspberryPi.cmake
+++ tools/toolchains/Toolchain-RaspberryPi.cmake
@@ -4,9 +4,9 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_SYSROOT /home/devel/rasp-pi-rootfs)
 set(CMAKE_STAGING_PREFIX /home/devel/stage)
 
-set(tools /home/devel/gcc-4.7-aarch64-linux-gnu)
-set(CMAKE_C_COMPILER ${tools}/bin/gcc-aarch64-linux-gcc)
-set(CMAKE_CXX_COMPILER ${tools}/bin/gcc-aarch64-linux-g++)
+set(tools /usr)
+set(CMAKE_C_COMPILER ${tools}/bin/aarch64-linux-gnu-gcc-9)
+set(CMAKE_CXX_COMPILER ${tools}/bin/aarch64-linux-gnu-g++-9)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
