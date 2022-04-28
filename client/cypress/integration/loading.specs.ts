42d4e026ed239c08f47e5fe1d4af7e52603eda93
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-04-28 02:08:04 +0200

    Add split tests cypress and update CI

--- client/cypress/integration/loading.specs.ts
+++ client/cypress/integration/loading.specs.ts
@@ -6,81 +6,8 @@
 import { expect } from "chai";
 import specTitle from "cypress-sonarqube-reporter/specTitle";
 import { CyHttpMessages } from "cypress/types/net-stubbing";
-
-const AUTHOR = "MGuillaumeF";
-const PROJECT = "Hangman Game";
-const VERSION = "v0.1.0";
-
-function addPage(id: string, ...elements: any[]) {
-  return cy.document().then(($document) => {
-    cy.get("body").then(($div) => {
-      function add() {
-        const page = $document.createElement("div");
-        page.id = `cypress-application-${id}`;
-        page.style.backgroundColor = "#333";
-        page.style.color = "#FFF";
-        page.style.height = "100vh";
-        page.style.width = "100vw";
-        page.style.position = "fixed";
-        page.style.display = "flex";
-        page.style.alignItems = "center";
-        page.style.justifyContent = "center";
-        page.style.flexDirection = "column";
-        page.style.zIndex = "3000000";
-
-        elements.forEach((element) => page.append(element));
-        return page;
-      }
-      $div.empty();
-      $div.append(add);
-    });
-  });
-}
-
-function addPresentation(
-  title: string,
-  project: string = PROJECT,
-  version: string = VERSION,
-  author: string = AUTHOR
-) {
-  return cy.document().then(($document) => {
-    const mainTitle = $document.createElement("h1");
-    mainTitle.textContent = project;
-
-    const subTitle = $document.createElement("h2");
-    subTitle.textContent = title;
-
-    const pVersion = $document.createElement("p");
-    pVersion.textContent = version;
-
-    const date = $document.createElement("span");
-    date.textContent = new Date().toISOString().split("T")[0];
-    date.style.position = "fixed";
-    date.style.bottom = "0.5em";
-    date.style.right = "0.5em";
-
-    const sAuthor = $document.createElement("span");
-    sAuthor.textContent = author;
-    sAuthor.style.position = "fixed";
-    sAuthor.style.top = "0.5em";
-    sAuthor.style.left = "0.5em";
-
-    return addPage("pres", mainTitle, subTitle, pVersion, date, sAuthor);
-  });
-}
-
-function addEnd() {
-  return cy.document().then(($document) => {
-    const title = $document.createElement("h1");
-    title.textContent = "FIN";
-    return addPage("end", title);
-  });
-}
-
-function basicInputType(id: string, value: string) {
-  cy.get(`label[for=${id}]`).click();
-  return cy.get(`#${id}`).click().type(value).should("have.value", value);
-}
+import { basicInputType } from "./components/BasicInputs";
+import { addEnd, addPresentation } from "./presentation";
 
 describe(specTitle("Offlines Access"), () => {
   it("Settings", () => {
