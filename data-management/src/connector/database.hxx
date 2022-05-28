35c8cd3ec6c7f54e19c13b265283e98c9c0fa8cc
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-05-29 01:30:34 +0200

    Correction sonar

--- data-management/src/connector/database.hxx
+++ data-management/src/connector/database.hxx
@@ -33,7 +33,7 @@
 #  error unknown database; did you forget to define the DATABASE_* macros?
 #endif
 
-inline std::auto_ptr<odb::database>
+inline std::unique_ptr<odb::database>
 create_database (int& argc, char* argv[])
 {
   using namespace std;
@@ -60,9 +60,9 @@ create_database (int& argc, char* argv[])
   }
 
 #if defined(DATABASE_MYSQL)
-  auto_ptr<database> db (new odb::mysql::database (argc, argv));
+  unique_ptr<database> db (new odb::mysql::database (argc, argv));
 #elif defined(DATABASE_SQLITE)
-  auto_ptr<database> db (
+  unique_ptr<database> db (
      new odb::sqlite::database (
        argc, argv, false, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE));
   // Create the database schema. Due to bugs in SQLite foreign key
@@ -81,11 +81,11 @@ create_database (int& argc, char* argv[])
     c->execute ("PRAGMA foreign_keys=ON");
   }
 #elif defined(DATABASE_PGSQL)
-  auto_ptr<database> db (new odb::pgsql::database (argc, argv));
+  unique_ptr<database> db (new odb::pgsql::database (argc, argv));
 #elif defined(DATABASE_ORACLE)
-  auto_ptr<database> db (new odb::oracle::database (argc, argv));
+  unique_ptr<database> db (new odb::oracle::database (argc, argv));
 #elif defined(DATABASE_MSSQL)
-  auto_ptr<database> db (new odb::mssql::database (argc, argv));
+  unique_ptr<database> db (new odb::mssql::database (argc, argv));
 #endif
 
   return db;
