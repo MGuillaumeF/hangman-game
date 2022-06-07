4b289916f7859560cac48faa09e0ee03541abf84
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-06-07 18:33:30 +0000

    [AUTO] clang format write

--- data-management/src/model/user.hxx
+++ data-management/src/model/user.hxx
@@ -7,153 +7,129 @@
 
 /**
  * @brief class of Users in model
- * 
+ *
  */
 #pragma db object
 class user {
 public:
   /**
    * @brief Construct a new user object
-   * 
+   *
    */
-  user()=default;
+  user() = default;
 
   /**
    * @brief Get the Id object
-   * 
-   * @return const uint32_t& 
+   *
+   * @return const uint32_t&
    */
-  const uint32_t& getId () const {
-    return m_id;
-  };
+  const uint32_t &getId() const { return m_id; };
 
   /**
    * @brief Set the Id object
-   * 
-   * @param id 
+   *
+   * @param id
    */
-  void setId(const uint32_t& id) {
-    m_id = id; 
-  };
+  void setId(const uint32_t &id) { m_id = id; };
 
   /**
    * @brief Get the Login object
-   * 
-   * @return const std::string& 
+   *
+   * @return const std::string&
    */
-  const std::string& getLogin () const {
-    return m_login;
-  };
+  const std::string &getLogin() const { return m_login; };
 
   /**
    * @brief Set the Login object
-   * 
-   * @param login 
+   *
+   * @param login
    */
-  void setLogin(const std::string& login) {
-    m_login = login; 
-  };
+  void setLogin(const std::string &login) { m_login = login; };
 
   /**
    * @brief Get the Password object
-   * 
-   * @return const std::string& 
+   *
+   * @return const std::string&
    */
-  const std::string& getPassword () const {
-    return m_password;
-  };
+  const std::string &getPassword() const { return m_password; };
 
   /**
    * @brief Set the Password object
-   * 
-   * @param password 
+   *
+   * @param password
    */
-  void setPassword(const std::string& password) {
-    m_password = password;
-  };
+  void setPassword(const std::string &password) { m_password = password; };
 
   /**
    * @brief Get the Salt User object
-   * 
-   * @return const std::string& 
+   *
+   * @return const std::string&
    */
-  const std::string& getSaltUser () const {
-    return m_saltUser;
-  };
+  const std::string &getSaltUser() const { return m_saltUser; };
 
   /**
    * @brief Set the Salt User object
-   * 
-   * @param saltUser 
+   *
+   * @param saltUser
    */
-  void setSaltUser(const std::string& saltUser) {
-    m_saltUser = saltUser;
-  };
+  void setSaltUser(const std::string &saltUser) { m_saltUser = saltUser; };
 
   /**
    * @brief Get the Salt Session object
-   * 
-   * @return const std::string& 
+   *
+   * @return const std::string&
    */
-  const std::string& getSaltSession () const {
-    return m_saltSession;
-  };
+  const std::string &getSaltSession() const { return m_saltSession; };
 
   /**
    * @brief Set the Salt Session object
-   * 
-   * @param saltSession 
+   *
+   * @param saltSession
    */
-  void setSaltSession(const std::string& saltSession) {
+  void setSaltSession(const std::string &saltSession) {
     m_saltSession = saltSession;
   };
 
   /**
    * @brief Get the Token object
-   * 
-   * @return const std::string& 
+   *
+   * @return const std::string&
    */
-  const std::string& getToken () const {
-    return m_token;
-  };
+  const std::string &getToken() const { return m_token; };
 
   /**
    * @brief Set the Token object
-   * 
-   * @param token 
+   *
+   * @param token
    */
-  void setToken(const std::string& token) {
-    m_token = token;
-  };
+  void setToken(const std::string &token) { m_token = token; };
   /**
    * @brief Get the Last connection object
-   * 
-   * @return const uint32_t& 
+   *
+   * @return const uint32_t&
    */
-  const uint32_t& getLastConnection () const {
-    return m_lastConnection;
-  };
+  const uint32_t &getLastConnection() const { return m_lastConnection; };
 
   /**
    * @brief Set the last connection object
-   * 
-   * @param lastConnection 
+   *
+   * @param lastConnection
    */
-  void setLastConnection(const uint32_t& lastConnection) {
+  void setLastConnection(const uint32_t &lastConnection) {
     m_lastConnection = lastConnection;
   };
 
 private:
   friend class odb::access;
 
-  #pragma db id auto
+#pragma db id auto
   uint32_t m_id;
 
-   #pragma db options() options("CHECK(login != '')")
+#pragma db options() options("CHECK(login != '')")
   std::string m_login;
-   #pragma db options() options("CHECK(password != '')")
+#pragma db options() options("CHECK(password != '')")
   std::string m_password;
-   #pragma db options() options("CHECK(saltUser != '')")
+#pragma db options() options("CHECK(saltUser != '')")
   std::string m_saltUser;
   std::string m_saltSession;
   std::string m_token;
@@ -163,9 +139,8 @@ private:
 #pragma db object(user)
 
 #pragma db view object(user)
-struct user_stat
-{
-  #pragma db column("count(" + user::m_id + ")")
+struct user_stat {
+#pragma db column("count(" + user::m_id + ")")
   std::size_t count;
 };
 
