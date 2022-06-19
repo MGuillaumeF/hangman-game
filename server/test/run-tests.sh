5321685d55815a0e83ff62e60fce1c79788173b7
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-06-19 20:06:37 +0200

    Feature/add subdirectory (#612)
    divide CMakeLists.txt server and data management

--- server/test/run-tests.sh
+++ server/test/run-tests.sh
@@ -1,10 +1,12 @@
-cd ../bin
+cd ../../bin/server/bin
 ./HangmanGame 2>&1 > ./integration-tests-report.log &
 sleep 15
 
+
+cd ../../
 echo "tests START"
 
-ctest --verbose || (cat ../reports/logUT.xml && cat ../reports/logIT.xml && cat Testing/Temporary/LastTest.log && exit 1)
+ctest --verbose || (cat ../server/reports/logUT.xml && cat ../server/reports/logIT.xml && cat Testing/Temporary/LastTest.log && exit 1)
 
 sleep 60
 
