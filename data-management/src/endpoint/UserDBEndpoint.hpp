43ba4c551c2bc9b30980915825d7d4cbe3c4697e
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-06-22 20:08:21 +0200

    Update UserDBEndpoint.hpp

--- data-management/src/endpoint/UserDBEndpoint.hpp
+++ data-management/src/endpoint/UserDBEndpoint.hpp
@@ -45,7 +45,7 @@ class UserDBEndpoint {
    * @brief Construct a new User DB Endpoint object
    *
    */
-  explicit UserDBEndpoint(odb::core::database *const db);
+  explicit UserDBEndpoint(odb::core::database * db);
 
 public:
   /**
@@ -60,7 +60,7 @@ public:
    * @param db The database access pointer
    * @return UserDBEndpoint*
    */
-  static UserDBEndpoint *getInstance(odb::core::database *const db = nullptr);
+  static UserDBEndpoint *getInstance(odb::core::database * db = nullptr);
 
   /**
    * @brief Create a User object
