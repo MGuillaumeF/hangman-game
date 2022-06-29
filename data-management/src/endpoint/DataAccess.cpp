a0d746a8c7a2e7ccb730ff54592e5f3bacb55e24
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-06-29 01:25:35 +0000

    [AUTO] clang format write

--- data-management/src/endpoint/DataAccess.cpp
+++ data-management/src/endpoint/DataAccess.cpp
@@ -29,7 +29,7 @@ std::shared_ptr<odb::core::database> DataAccess::getDataBase() const {
  */
 std::shared_ptr<odb::core::database> DataAccess::getDatabaseAccess() {
   if (nullptr == s_instance) {
-     s_instance = new DataAccess();
-   }
+    s_instance = new DataAccess();
+  }
   return s_instance->getDataBase();
 }
