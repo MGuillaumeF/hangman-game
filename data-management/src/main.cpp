cc038628c6bb774e58789e65271ba9a2577d3a42
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-06-30 02:55:16 +0200

    Update main.cpp

--- data-management/src/main.cpp
+++ data-management/src/main.cpp
@@ -9,19 +9,11 @@
 #include <sstream> // std::stringstream
 #include <string>  // std::string
 
-#include <boost/property_tree/ptree.hpp>
-
-// create database access
-#include "./endpoint/DataAccess.hpp"
-#include "./endpoint/UserDBEndpoint.hpp"
-
 #include "./TCP/Server.hpp"
 
 int32_t main(int argc, char *argv[]) {
   int32_t exitStatus = EXIT_SUCCESS;
   try {
-    std::shared_ptr<odb::core::database> db = DataAccess::getDatabaseAccess();
-    UserDBEndpoint::getInstance(db);
     boost::asio::io_context ioContext{3};
     const hangman::tcp::Server server(ioContext, 50000);
     ioContext.run();
