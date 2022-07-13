6a449a53cf55d5c84a59009979570c6166248963
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-07-13 11:13:23 +0000

    [AUTO] clang format write

--- data-management/src/TCP/Client.hpp
+++ data-management/src/TCP/Client.hpp
@@ -1,10 +1,9 @@
 #ifndef __TCP_CLIENT_HPP__
 #define __TCP_CLIENT_HPP__
 
-#include <string>  // std::string
+#include <string> // std::string
 
 #include <boost/property_tree/ptree.hpp>
-
 
 namespace hangman {
 namespace tcp {
@@ -28,7 +27,7 @@ public:
    * @return The data manager response
    */
   static boost::property_tree::ptree sendRequest(const std::string &hostname,
-                                         const uint16_t &port,
+                                                 const uint16_t &port,
                                                  const std::string filename);
 };
 
