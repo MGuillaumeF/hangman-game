8e8dd993582fe01eb000284d413c2d228ad038c1
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-07-20 21:10:51 +0200

    Update testUserEndpointNominal.cpp

--- data-management/test/ut/testUserEndpointNominal.cpp
+++ data-management/test/ut/testUserEndpointNominal.cpp
@@ -42,12 +42,12 @@ BOOST_AUTO_TEST_CASE(test_create_one) {
   }
 
   uint8_t counter = 0;
+  /*
   for (const auto &[nodeName, nodeContent] : response) {
     if (nodeName == "user") {
       counter += 1;
     }
   }
-  /*
   BOOST_CHECK_EQUAL(1, counter);
   */
 }
