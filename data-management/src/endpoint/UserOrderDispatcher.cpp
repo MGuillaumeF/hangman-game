8724744c182f12a80f7ab6b343c0c8a5d0cb130b
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-07-20 19:58:34 +0200

    Clean sonar (#703)
    * Update UserOrderDispatcher.cpp
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write

--- data-management/src/endpoint/UserOrderDispatcher.cpp
+++ data-management/src/endpoint/UserOrderDispatcher.cpp
@@ -14,19 +14,23 @@ boost::property_tree::ptree
 UserOrderDispatcher::route(const std::string &orderType,
                            const boost::property_tree::ptree &properties,
                            const boost::property_tree::ptree &data) {
+  // response data property tree
   boost::property_tree::ptree response;
 
   std::cout << "ORDER TYPE : " << properties.get<std::string>("order-type")
             << std::endl;
   if ("create" == orderType) {
+    // create users from data of property tree
     response = UserDBEndpoint::getInstance()->createUser(data);
   } else if ("read" == orderType) {
-
+    // read users from database, by key of property tree
   } else if ("update" == orderType) {
-
+    // full update of users
   } else if ("delete" == orderType) {
+    // delete users
     UserDBEndpoint::getInstance()->deleteUser(data);
   } else if ("patch" == orderType) {
+    // partial update of users
   }
   return response;
 }
