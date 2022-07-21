965c4638d9721e36400f10c13e8af6dc8d4454f0
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-07-21 14:55:24 +0200

    Update main.cpp (#711)
    * Update main.cpp
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write
    
    * Update main.cpp

--- data-management/src/main.cpp
+++ data-management/src/main.cpp
@@ -1,23 +1,21 @@
 #include <exception>
-#include <fstream>
 #include <iostream> // std::cout
-#include <memory>   // unique_ptr
-#include <thread>
 
-#include <chrono>
-#include <iomanip>
-#include <sstream> // std::stringstream
-#include <string>  // std::string
-
+// import TCP Server
 #include "./TCP/Server.hpp"
 
 int32_t main(int argc, char *argv[]) {
+  // by default process exit status is success
   int32_t exitStatus = EXIT_SUCCESS;
   try {
+    // create io context for server
     boost::asio::io_context ioContext{3};
+    // attach tcp server on io context and declare it on 50000 port
     const hangman::tcp::Server server(ioContext, 50000);
+    // start tcp server
     ioContext.run();
   } catch (const std::exception &e) {
+    // if error raised log it and exit code is changed to failure
     std::cerr << "Exception: " << e.what() << "\n";
     exitStatus = EXIT_FAILURE;
   }
