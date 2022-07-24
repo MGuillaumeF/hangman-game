93f2237b6619093281783b11f059c97b0453d4f7
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-07-24 12:57:47 +0200

    Update root_model_object.hxx

--- data-management/src/model/root_model_object.hxx
+++ data-management/src/model/root_model_object.hxx
@@ -34,6 +34,41 @@ public:
   void setId(const uint32_t &id) { m_id = id; };
 
   /**
+   * @brief Get the version of object
+   *
+   * @return const uint32_t& the version of object
+   */
+  const uint32_t &getVersion() const { return m_version; };
+
+  /**
+   * @brief Get the creation user of object
+   *
+   * @return const std::string& the creation user of object
+   */
+  const std::string &getCreatedBy() const { return m_created_by; };
+
+  /**
+   * @brief Get the creation timestamp of object
+   *
+   * @return const uint32_t& the creation timestamp of object
+   */
+  const uint32_t &getCreatedAt() const { return m_created_at; };
+
+  /**
+   * @brief Get the updater user of object
+   *
+   * @return const std::string& the updater user of object
+   */
+  const std::string &getUpdatedBy() const { return m_updated_by; };
+
+  /**
+   * @brief Get the update timestamp of object
+   *
+   * @return const uint32_t& the update timestamp of object
+   */
+  const uint32_t &getUpdatedAt() const { return m_updated_at; };
+
+  /**
    * @brief method to check if all fields of object are valid
    *
    * @return true The content of object is valid
