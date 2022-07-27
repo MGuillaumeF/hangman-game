3c2c0b16bf9298cb344e4ba8d5c3186ab0d9c05c
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-07-27 21:07:52 +0000

    [AUTO] clang format write

--- data-management/src/model/group.hxx
+++ data-management/src/model/group.hxx
@@ -100,8 +100,8 @@ private:
 
 #pragma db options() options("CHECK(name != '')")
   std::string m_name;
-#pragma db value_not_null 
-// inverse(m_groups)
+#pragma db value_not_null
+  // inverse(m_groups)
   std::vector<std::shared_ptr<user>> m_members;
 };
 
