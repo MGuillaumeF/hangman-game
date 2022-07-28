d393af2c39c218176d1067cef674b6c0098947ca
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-07-28 02:51:14 +0200

    Update group.hxx

--- data-management/src/model/group.hxx
+++ data-management/src/model/group.hxx
@@ -40,9 +40,9 @@ public:
   /**
    * @brief Get members of group object
    *
-   * @return const std::vector<std::shared_ptr<user>>&
+   * @return const std::vector<std::weak_ptr<user>>&
    */
-  const std::vector<std::shared_ptr<user>> &getMembers() const {
+  const std::vector<std::weak_ptr<user>> &getMembers() const {
     return m_members;
   };
 
@@ -51,7 +51,7 @@ public:
    *
    * @param members
    */
-  void setMembers(const std::vector<std::shared_ptr<user>> &members) {
+  void setMembers(const std::vector<std::weak_ptr<user>> &members) {
     m_members = members;
   };
 
@@ -102,7 +102,7 @@ private:
 #pragma db options() options("CHECK(name != '')")
   std::string m_name;
 #pragma db value_not_null inverse(m_groups)
-  std::vector<std::shared_ptr<user>> m_members;
+  std::vector<std::weak_ptr<user>> m_members;
 };
 
 #pragma db object(group)
