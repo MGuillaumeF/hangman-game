76e49277b944eaad2d43cc55637fb5e28a8e32ae
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-07 19:05:07 +0200

    Create StatusCode.hpp

--- data-management/src/endpoint/StatusCode.hpp
+++ data-management/src/endpoint/StatusCode.hpp
@@ -1,12 +1,10 @@
+#ifndef __SATUS_CODE_HPP__
+#define __SATUS_CODE_HPP__
+
+
 /**
  * @brief all status code
  */
-enum class status_code {
-  OK = 200,
-  BAD_REQUEST = 400,
-  NOT_FOUND = 404,
-  METHOD_NOT_ALLOWED = 405,
-  CONFLICT = 409,
-  INTERNAL_SERVER_ERROR = 500,
-  SERVICE_UNAVAILABLE = 503
-};
+enum class status_code { OK = 200, BAD_REQUEST = 400, NOT_FOUND = 404, METHOD_NOT_ALLOWED = 405, CONFLICT = 409, INTERNAL_SERVER_ERROR = 500, SERVICE_UNAVAILABLE = 503 };
+
+#endif
