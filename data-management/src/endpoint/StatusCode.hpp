149546fde23ff4521901e94a1c79800fa4094944
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-07 17:04:29 +0000

    [AUTO] clang format write

--- data-management/src/endpoint/StatusCode.hpp
+++ data-management/src/endpoint/StatusCode.hpp
@@ -1,4 +1,12 @@
 /**
  * @brief all status code
  */
-enum class status_code { OK = 200, BAD_REQUEST = 400, NOT_FOUND = 404, METHOD_NOT_ALLOWED = 405, CONFLICT = 409, INTERNAL_SERVER_ERROR = 500, SERVICE_UNAVAILABLE = 503 };
+enum class status_code {
+  OK = 200,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  METHOD_NOT_ALLOWED = 405,
+  CONFLICT = 409,
+  INTERNAL_SERVER_ERROR = 500,
+  SERVICE_UNAVAILABLE = 503
+};
