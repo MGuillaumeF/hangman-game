9ea954481e3805b3cb3a9c7d861de63c94f284f5
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-07 19:08:49 +0200

    Update GroupeOrderDispatcher.cpp

--- data-management/src/endpoint/GroupeOrderDispatcher.cpp
+++ data-management/src/endpoint/GroupeOrderDispatcher.cpp
@@ -20,7 +20,9 @@ GroupeOrderDispatcher::route(const std::string &groupeId,
   } else if ("CUSTOM" == groupeId) {
 
   } else {
-    // unknown order groupe
+    // unknown order group
+    // unknown object type
+    response.put("status-code", status_code::NOT_FOUND);
   }
   return response;
 }
