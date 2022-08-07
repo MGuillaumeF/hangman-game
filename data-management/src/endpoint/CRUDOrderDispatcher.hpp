2ca6938c3792e2f03efaa809da208df6b7c5405c
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-07 19:07:25 +0200

    Update CRUDOrderDispatcher.hpp

--- data-management/src/endpoint/CRUDOrderDispatcher.hpp
+++ data-management/src/endpoint/CRUDOrderDispatcher.hpp
@@ -2,6 +2,7 @@
 #define __CRUD_ORDER_DISPATCHER_HPP__
 
 #include "../model/root_model_object.hxx"
+#include "./StatusCode.hpp"
 // generated configuration
 #include "./DataAccess.hpp"
 #include "config.hpp"
@@ -80,7 +81,7 @@ public:
       // partial update of users
       response = updateObject<T>(properties, data, false);
     } else {
-      response.put("status-code", 405);
+      response.put("status-code", status_code::METHOD_NOT_ALLOWED);
     }
     return response;
   }
@@ -145,7 +146,7 @@ public:
         objectId.put("id", id);
         response.add_child(T::getObjectType(), objectId);
       }
-      response.put("status-code", 200);
+      response.put("status-code", status_code::OK);
       t.commit();
     } else {
       response.add_child("errors", errors);
@@ -223,7 +224,7 @@ public:
       DataAccess::getDatabaseAccess()->erase<T>(objectToDelete);
     }
     t.commit();
-    response.put("status-code", 200);
+    response.put("status-code", status_code::OK);
     return response;
   }
 };
