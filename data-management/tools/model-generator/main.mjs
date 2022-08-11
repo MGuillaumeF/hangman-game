b5231b527e18ed2455c67fbeabdb489ee3646535
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-12 01:29:11 +0200

    Update main.mjs

--- data-management/tools/model-generator/main.mjs
+++ data-management/tools/model-generator/main.mjs
@@ -2,6 +2,11 @@
 import { parseString } from "xml2js";
 import { readFileSync } from "fs";
 import path from "path";
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const xml = readFileSync(path.resolve(__dirname, "../model.xml"));
 parseString(xml, function (err, result) {
     console.info("xml parsing result", JSON.stringify(result, null, 4));
