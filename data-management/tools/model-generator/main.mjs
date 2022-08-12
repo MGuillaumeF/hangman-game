8b8b4bf5b47c40733b743c661d8bdf4096c0edfb
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-12 13:27:37 +0200

    Feature/add prettier other node project (#788)
    * Update default.yml
    
    * Create .gitignore
    
    * [AUTO] prettier format write

--- data-management/tools/model-generator/main.mjs
+++ data-management/tools/model-generator/main.mjs
@@ -1,29 +1,28 @@
-
 import { parseString } from "xml2js";
 import { readFileSync } from "fs";
 import path from "path";
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const cppMapTypes = {
-  "date" : "std::time_t",
-  "string" : "std::string",
-  "int8" : "int8_t",
-  "int16" : "int16_t",
-  "int32" : "int32_t",
-  "int64" : "int64_t",
-  "uint8" : "uint8_t",
-  "uint16" : "uint16_t",
-  "uint32" : "uint32_t",
-  "uint64" : "uint64_t"
-}
+  date: "std::time_t",
+  string: "std::string",
+  int8: "int8_t",
+  int16: "int16_t",
+  int32: "int32_t",
+  int64: "int64_t",
+  uint8: "uint8_t",
+  uint16: "uint16_t",
+  uint32: "uint32_t",
+  uint64: "uint64_t",
+};
 
 const cppMapIncludes = {
-  "date" : "ctime",
-  "string" : "string"
-}
+  date: "ctime",
+  string: "string",
+};
 
 function toCapitalize(str) {
   return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
@@ -31,12 +30,12 @@ function toCapitalize(str) {
 
 function snakeCaseToCamelCase(str) {
   const words = str.split("_");
-  return `${words[0]}${words.slice(1).map(toCapitalize).join('')}`;
+  return `${words[0]}${words.slice(1).map(toCapitalize).join("")}`;
 }
 
 function snakeCaseToUpperCamelCase(str) {
   const words = str.split("_");
-  return words.map(toCapitalize).join('');
+  return words.map(toCapitalize).join("");
 }
 
 function generateClasses(modelClasses) {
@@ -52,23 +51,29 @@ function generateCppSetter(attrData) {
    *
    * @param ${attrData.name} The ${attrData.name} of object
    */
-  void set${snakeCaseToUpperCamelCase(attrData.name)}(const ${cppMapTypes[attrData.type] ? cppMapTypes[attrData.type] : attrData.type } &${attrData.name}) { m_${attrData.name} = ${attrData.name}; };`
+  void set${snakeCaseToUpperCamelCase(attrData.name)}(const ${
+    cppMapTypes[attrData.type] ? cppMapTypes[attrData.type] : attrData.type
+  } &${attrData.name}) { m_${attrData.name} = ${attrData.name}; };`;
 }
 
 function generateCppGetter(attrData) {
   return `/**
    * @brief Get the ${attrData.name} of object
    *
-   * @return const ${cppMapTypes[attrData.type] ? cppMapTypes[attrData.type] : attrData.type }& the ${attrData.name} of object
+   * @return const ${
+     cppMapTypes[attrData.type] ? cppMapTypes[attrData.type] : attrData.type
+   }& the ${attrData.name} of object
    */
-  const ${cppMapTypes[attrData.type] ? cppMapTypes[attrData.type] : attrData.type } &get${snakeCaseToUpperCamelCase(attrData.name)}() const { return m_${attrData.name}; };`
-
+  const ${
+    cppMapTypes[attrData.type] ? cppMapTypes[attrData.type] : attrData.type
+  } &get${snakeCaseToUpperCamelCase(attrData.name)}() const { return m_${
+    attrData.name
+  }; };`;
 }
 
-
 function generateCppClass(modelClass) {
-  const className = modelClass.$.name
-  const extendClass = modelClass.$.extend
+  const className = modelClass.$.name;
+  const extendClass = modelClass.$.extend;
   const filename = `${className}.hxx`;
   const guard = `__${className.toUpperCase()}_HXX__`;
 
@@ -76,39 +81,54 @@ function generateCppClass(modelClass) {
 
   const includesCpp = new Set();
   const attributes = modelClass.attributes[0].attribute;
-  const privateAttributes = attributes.filter(attributeObject => {
-    return attributeObject.$.visibility == "private";
-  }).map(attributeObject => {
-    const attrData = attributeObject.$;
-    assessors.push(generateCppSetter(attrData));
-    assessors.push(generateCppGetter(attrData));
-    if (cppMapIncludes[attrData.type]) {
-      includesCpp.add(cppMapIncludes[attrData.type]);
-    }
-    return `${cppMapTypes[attrData.type] ? cppMapTypes[attrData.type] : attrData.type } m_${attrData.name};`
-  }).join("\n");
-  const protectedAttributes = attributes.filter(attributeObject => {
-    return attributeObject.$.visibility == "protected";
-  }).map(attributeObject => {
-    const attrData = attributeObject.$;
-    assessors.push(generateCppSetter(attrData));
-    assessors.push(generateCppGetter(attrData));
-    if (cppMapIncludes[attrData.type]) {
-      includesCpp.add(cppMapIncludes[attrData.type]);
-    }
-    return `${cppMapTypes[attrData.type] ? cppMapTypes[attrData.type] : attrData.type } m_${attrData.name};`
-  }).join("\n");
-  const publicAttributes = attributes.filter(attributeObject => {
-    return attributeObject.$.visibility == "public";
-  }).map(attributeObject => {
-    const attrData = attributeObject.$;
-    assessors.push(generateCppSetter(attrData));
-    assessors.push(generateCppGetter(attrData));
-    if (cppMapIncludes[attrData.type]) {
-      includesCpp.add(cppMapIncludes[attrData.type]);
-    }
-    return `${cppMapTypes[attrData.type] ? cppMapTypes[attrData.type] : attrData.type } m_${attrData.name};`
-  }).join("\n");
+  const privateAttributes = attributes
+    .filter((attributeObject) => {
+      return attributeObject.$.visibility == "private";
+    })
+    .map((attributeObject) => {
+      const attrData = attributeObject.$;
+      assessors.push(generateCppSetter(attrData));
+      assessors.push(generateCppGetter(attrData));
+      if (cppMapIncludes[attrData.type]) {
+        includesCpp.add(cppMapIncludes[attrData.type]);
+      }
+      return `${
+        cppMapTypes[attrData.type] ? cppMapTypes[attrData.type] : attrData.type
+      } m_${attrData.name};`;
+    })
+    .join("\n");
+  const protectedAttributes = attributes
+    .filter((attributeObject) => {
+      return attributeObject.$.visibility == "protected";
+    })
+    .map((attributeObject) => {
+      const attrData = attributeObject.$;
+      assessors.push(generateCppSetter(attrData));
+      assessors.push(generateCppGetter(attrData));
+      if (cppMapIncludes[attrData.type]) {
+        includesCpp.add(cppMapIncludes[attrData.type]);
+      }
+      return `${
+        cppMapTypes[attrData.type] ? cppMapTypes[attrData.type] : attrData.type
+      } m_${attrData.name};`;
+    })
+    .join("\n");
+  const publicAttributes = attributes
+    .filter((attributeObject) => {
+      return attributeObject.$.visibility == "public";
+    })
+    .map((attributeObject) => {
+      const attrData = attributeObject.$;
+      assessors.push(generateCppSetter(attrData));
+      assessors.push(generateCppGetter(attrData));
+      if (cppMapIncludes[attrData.type]) {
+        includesCpp.add(cppMapIncludes[attrData.type]);
+      }
+      return `${
+        cppMapTypes[attrData.type] ? cppMapTypes[attrData.type] : attrData.type
+      } m_${attrData.name};`;
+    })
+    .join("\n");
 
   const cppClassTemplate = `
 /**
@@ -119,14 +139,16 @@ function generateCppClass(modelClass) {
 #ifndef ${guard}
 #define ${guard} 
 
-${Array.from(includesCpp).map(inc => `#include <${inc}>`).join("\n")}
+${Array.from(includesCpp)
+  .map((inc) => `#include <${inc}>`)
+  .join("\n")}
 
 /**
  * @brief class of ${className} object in model
  *
  */
 #pragma db object
-class ${className} ${extendClass ? `final : public ${extendClass}` : ''} {
+class ${className} ${extendClass ? `final : public ${extendClass}` : ""} {
 private:
 ${privateAttributes}
 
@@ -148,17 +170,13 @@ ${assessors.join("\n\n")}
 #endid // end ${guard}`;
 
   console.info("generated :", cppClassTemplate);
-
 }
 function generateTsClass(modelClass) {
   // TODO add template implementation of typescript classes
 }
 
-
-
 const xml = readFileSync(path.resolve(__dirname, "../../resources/model.xml"));
 parseString(xml, function (err, result) {
-    console.info("xml parsing result", JSON.stringify(result, null, 1));
-    generateClasses(result.model.classes[0].class);
+  console.info("xml parsing result", JSON.stringify(result, null, 1));
+  generateClasses(result.model.classes[0].class);
 });
-
