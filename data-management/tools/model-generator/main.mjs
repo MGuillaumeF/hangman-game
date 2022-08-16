e8fa72e0e524300b66cb4e99e91cec4915761ba2
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-16 20:58:43 +0000

    [AUTO] prettier format write

--- data-management/tools/model-generator/main.mjs
+++ data-management/tools/model-generator/main.mjs
@@ -169,7 +169,7 @@ function generateCppClass(modelClass) {
 #define ${guard} 
 
 ${Array.from(includesModelObjectsCpp)
-  .filter(inc => inc !== className)
+  .filter((inc) => inc !== className)
   .map((inc) => `#include "./${inc}.hxx"`)
   .join("\n")}
 
