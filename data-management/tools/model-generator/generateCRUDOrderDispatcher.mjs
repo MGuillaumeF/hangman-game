59045634cbd5a1045fec43d29f6eef7409692963
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-18 08:43:57 +0200

    Feature/add crud dispatcher (#814)
    * Create generateCRUDOrderDispatcher.mjs
    
    * Update main.mjs
    
    * Update generateCRUDOrderDispatcher.mjs
    
    * Update main.mjs
    
    * Update generateCRUDOrderDispatcher.mjs
    
    * Update generateCRUDOrderDispatcher.mjs
    
    * Update generateCRUDOrderDispatcher.mjs
    
    * [AUTO] prettier format write
    
    * Create CRUDOrderDispatcher.tlat
    
    * [AUTO] prettier format write
    
    * Create loader.mjs
    
    * Update generateCRUDOrderDispatcher.mjs
    
    * Update generateCRUDOrderDispatcher.mjs
    
    * Update loader.mjs
    
    * Update loader.mjs
    
    * Update loader.mjs
    
    * [AUTO] prettier format write
    
    * Create loader.mjs
    
    * Update loader.mjs
    
    * Update loader.mjs
    
    * [AUTO] prettier format write
    
    * Update loader.mjs
    
    * [AUTO] prettier format write
    
    * Update loader.mjs
    
    * Update generateCRUDOrderDispatcher.mjs
    
    * Update generateCRUDOrderDispatcher.mjs
    
    * [AUTO] prettier format write

---
+++ data-management/tools/model-generator/generateCRUDOrderDispatcher.mjs
@@ -0,0 +1,88 @@
+import { load } from "./loader.mjs";
+
+function generateDatabaseImport(databaseType, classNames) {
+  const importObjectList = [];
+  for (const className of classNames) {
+    importObjectList.push(
+      `#include "../model/${databaseType}/${className}-odb.hxx"`,
+      `#include "../model/${databaseType}/${className}.hxx"`
+    );
+  }
+  return importObjectList.join("\n");
+}
+
+function generatrDatabaseImportBloc(databaseTypes, classNames) {
+  const blocString = databaseTypes.map(
+    (databaseType, index) => `
+#${index === 0 ? "" : "el"}if defined(DATABASE_${databaseType.toUpperCase()})
+${generateDatabaseImport(databaseType, classNames)}
+`
+  );
+  blocString.push(`
+#else
+#error unknown database; did you forget to define the DATABASE_* macros?
+#endif`);
+  return blocString.join("");
+}
+
+function generateObjectTypesDispatcher(classNames) {
+  const conditionList = Array.from(classNames)
+    .filter((className) => className !== "root_model_object")
+    .map((className, index) => {
+      return `${index === 0 ? "" : "else"} if ("${className}" == objectType) {
+    // dispatch crud ${className} orders
+    response = routeObjectType<${className}>(properties.get<std::string>("order-type"),
+                                     properties, data);
+  }`;
+    });
+  return conditionList.join(" ");
+}
+
+export function generateCRUDOrderDispatcher(classNames) {
+  return load("CRUDOrderDispatcher", {
+    generatedDatabaseImport: generatrDatabaseImportBloc(
+      ["mysql", "sqlite", "pgsql"],
+      classNames
+    ),
+    generatedObjectTypesDispatcher: generateObjectTypesDispatcher(classNames),
+  });
+
+  return `
+#include "./CRUDOrderDispatcher.hpp"
+
+#if defined(DATABASE_MYSQL)
+${generateDatabaseImport("mysql", classNames)}
+#elif defined(DATABASE_SQLITE)
+${generateDatabaseImport("sqlite", classNames)}
+#elif defined(DATABASE_PGSQL)
+${generateDatabaseImport("pgsql", classNames)}
+#else
+#error unknown database; did you forget to define the DATABASE_* macros?
+#endif
+
+#include <iostream>
+
+/**
+ * @brief Route to dispatch crud order by object type
+ *
+ * @param objectType The object requested
+ * @param properties The properties part of request
+ * @param data The data part of request
+ * @return The result of request process
+ */
+boost::property_tree::ptree
+CRUDOrderDispatcher::route(const std::string &objectType,
+                           const boost::property_tree::ptree &properties,
+                           const boost::property_tree::ptree &data) {
+  boost::property_tree::ptree response;
+
+  std::cout << "OBJECT TYPE : " << properties.get<std::string>("object-type")
+            << std::endl;
+  ${generateObjectTypesDispatcher(classNames)} else {
+    // unknown object type
+    response.put("status-code", uint16_t(status_code::NOT_FOUND));
+  }
+  return response;
+}
+`;
+}
---
+++ data-management/tools/model-generator/loader.mjs
@@ -0,0 +1,57 @@
+import { readFileSync, existsSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export function load(templateName, parameters) {
+  const templatePath = path.resolve(
+    __dirname,
+    "templates",
+    `${templateName}.tlat`
+  );
+  let templateContent = "";
+  if (existsSync(templatePath)) {
+    templateContent = readFileSync(templatePath).toString();
+    const matches = templateContent.matchAll(/\{\{\s*(\S+)\s*\}\}/gm);
+    let usedVars = [];
+    if (matches) {
+      usedVars = [].concat(
+        ...Array.from(matches).map((match) => match.slice(1))
+      );
+      console.info("variables to replace are :", ...usedVars);
+    } else {
+      console.debug("no variables found");
+    }
+
+    const providedVars = Object.keys(parameters);
+    for (const usedVar of usedVars) {
+      if (providedVars.includes(usedVar)) {
+        const re = new RegExp(`\\{\\{\\s*${usedVar}\\s*\\}\\}`, "gm");
+        templateContent = templateContent.replace(re, parameters[usedVar]);
+      } else {
+        console.error(
+          "The parameter ",
+          usedVar,
+          " is not provided to the template"
+        );
+        //process.exit(1)
+      }
+    }
+
+    for (const providedVar of providedVars) {
+      if (!usedVars.includes(providedVar)) {
+        console.warn(
+          "The parameter ",
+          providedVar,
+          " is not used on the template"
+        );
+      }
+    }
+  } else {
+    console.error("template not found :", templatePath);
+    //process.exit(1);
+  }
+  return templateContent;
+}
