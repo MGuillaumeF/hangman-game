fca6c15b9a25a374d2938b234b50feec5a862a3b
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-18 04:06:58 +0200

    Create loader.mjs

--- data-management/tools/model-generator/loader.mjs
+++ data-management/tools/model-generator/loader.mjs
@@ -6,47 +6,36 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export function load(templateName, parameters) {
-  const templatePath = path.resolve(
-    __dirname,
-    "templates",
-    `${templateName}.tlat`
-  );
-  let templateContent = "";
+  const templatePath = path.resolve(__dirname, "templates", `${templateName}.tlat`);
+  let templateContent = '';
   if (existsSync(templatePath)) {
     templateContent = readFileSync(templatePath).toString();
-    const matches = templateContent.match(/^.*\{\{\s*(\S+)\s*\}\}.*$/g);
-    let usedVars = [];
+    const matches = templateContent.match(/^.*\{\{\s*(\S+)\s*\}\}.*$/gm);
+    let usedVars = []
     if (matches) {
-      usedVars = matches.slice(1);
-      console.info("variables to replace are :", ...matches.slice(1));
-    } else {
-      console.debug("no variables found");
-    }
-
+    usedVars = matches.slice(1)
+    console.info("variables to replace are :", ...matches.slice(1));
+} else {
+   console.debug("no variables found");
+}
+    
     const providedVars = Object.keys(parameters);
     for (const usedVar of usedVars) {
       if (providedVars.includes(usedVar)) {
-        const re = new RegExp(`/^.*\\{\\{\\s*(${usedVar})\\s*\\}\\}.*$/`, "g");
-        templateContent = templateContent.replace(re, parameters[usedVar]);
+        const re = new RegExp(`/^(.*)\\{\\{\\s*${usedVar}\\s*\\}\\}(.*)$/`, "gm");
+        templateContent = templateContent.replace(re, `$1${parameters[usedVar]}$2`);
       } else {
-        console.error(
-          "The parameter ",
-          usedVar,
-          " is not provided to the template"
-        );
-        process.exit(1);
+        console.error("The parameter ", usedVar, " is not provided to the template");
+        process.exit(1)
       }
     }
 
     for (const providedVar of providedVars) {
-      if (!usedVars.includes(providedVar)) {
-        console.warn(
-          "The parameter ",
-          providedVar,
-          " is not used on the template"
-        );
+      if(!usedVars.includes(providedVar)) {
+        console.warn("The parameter ", providedVar, " is not used on the template");
       }
     }
+    
   } else {
     console.error("template not found :", templatePath);
     process.exit(1);
