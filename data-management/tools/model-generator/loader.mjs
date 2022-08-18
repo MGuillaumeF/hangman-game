da895448603ace2758f70cb057fc400c5f4a678f
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-18 02:39:00 +0000

    [AUTO] prettier format write

--- data-management/tools/model-generator/loader.mjs
+++ data-management/tools/model-generator/loader.mjs
@@ -6,36 +6,47 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export function load(templateName, parameters) {
-  const templatePath = path.resolve(__dirname, "templates", `${templateName}.tlat`);
-  let templateContent = '';
+  const templatePath = path.resolve(
+    __dirname,
+    "templates",
+    `${templateName}.tlat`
+  );
+  let templateContent = "";
   if (existsSync(templatePath)) {
     templateContent = readFileSync(templatePath).toString();
     const matches = templateContent.matchAll(/\{\{\s*(\S+)\s*\}\}/gm);
-    let usedVars = []
+    let usedVars = [];
     if (matches) {
-    usedVars = Array.from(matches)
-    console.info("variables to replace are :", ...usedVars);
-} else {
-   console.debug("no variables found");
-}
-    
+      usedVars = Array.from(matches);
+      console.info("variables to replace are :", ...usedVars);
+    } else {
+      console.debug("no variables found");
+    }
+
     const providedVars = Object.keys(parameters);
     for (const usedVar of usedVars) {
       if (providedVars.includes(usedVar)) {
         const re = new RegExp(`/\\{\\{\\s*${usedVar}\\s*\\}\\}/`, "gm");
         templateContent = templateContent.replace(re, parameters[usedVar]);
       } else {
-        console.error("The parameter ", usedVar, " is not provided to the template");
+        console.error(
+          "The parameter ",
+          usedVar,
+          " is not provided to the template"
+        );
         //process.exit(1)
       }
     }
 
     for (const providedVar of providedVars) {
-      if(!usedVars.includes(providedVar)) {
-        console.warn("The parameter ", providedVar, " is not used on the template");
+      if (!usedVars.includes(providedVar)) {
+        console.warn(
+          "The parameter ",
+          providedVar,
+          " is not used on the template"
+        );
       }
     }
-    
   } else {
     console.error("template not found :", templatePath);
     //process.exit(1);
