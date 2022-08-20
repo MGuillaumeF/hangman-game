ed5ae7d0418afc1d6056de990873eac981632574
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-21 01:18:58 +0200

    Create utils.ts

---
+++ data-management/tools/model-generator/src/utils.ts
@@ -0,0 +1,13 @@
+export function toCapitalize(str) {
+  return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+}
+
+exportfunction snakeCaseToCamelCase(str) {
+  const words = str.split("_");
+  return `${words[0]}${words.slice(1).map(toCapitalize).join("")}`;
+}
+
+export function snakeCaseToUpperCamelCase(str) {
+  const words = str.split("_");
+  return words.map(toCapitalize).join("");
+}
