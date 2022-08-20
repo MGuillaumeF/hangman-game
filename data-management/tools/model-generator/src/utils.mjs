3b4a1796fcf136224cc79ba29712859e82c3f173
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-21 01:59:45 +0200

    Update utils.mjs

--- data-management/tools/model-generator/src/utils.mjs
+++ data-management/tools/model-generator/src/utils.mjs
@@ -2,7 +2,7 @@ export function toCapitalize(str) {
   return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
 }
 
-exportfunction snakeCaseToCamelCase(str) {
+export function snakeCaseToCamelCase(str) {
   const words = str.split("_");
   return `${words[0]}${words.slice(1).map(toCapitalize).join("")}`;
 }
