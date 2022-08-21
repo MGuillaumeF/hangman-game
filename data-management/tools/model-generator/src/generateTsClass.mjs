0f68f39748f4df5b21ad1423a22a74f1f270084e
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-21 13:52:58 +0000

    [AUTO] prettier format write

--- data-management/tools/model-generator/src/generateTsClass.mjs
+++ data-management/tools/model-generator/src/generateTsClass.mjs
@@ -1,21 +1,23 @@
 import { load } from "./loader.mjs";
 
 export function generateTsClass(modelClasses) {
-  console.log(load("TsClasses", {
-  dependencies : "",
-  className: "",
-  extendedClasses : "",
-  privateAttributes : "",
-  privateStaticAttributes:"",
-  privateMethods:"",
-  privateStaticMethods : "",
-  protectedAttributes : "",
-  protectedStaticAttributes : "",
-  protectedMethods : "",
-  protectedStaticMethods : "",
-  publicAttributes : "",
-  publicStaticAttributes : "",
-  publicMethods : "",
-  publicStaticMethods : ""
-}));
+  console.log(
+    load("TsClasses", {
+      dependencies: "",
+      className: "",
+      extendedClasses: "",
+      privateAttributes: "",
+      privateStaticAttributes: "",
+      privateMethods: "",
+      privateStaticMethods: "",
+      protectedAttributes: "",
+      protectedStaticAttributes: "",
+      protectedMethods: "",
+      protectedStaticMethods: "",
+      publicAttributes: "",
+      publicStaticAttributes: "",
+      publicMethods: "",
+      publicStaticMethods: "",
+    })
+  );
 }
