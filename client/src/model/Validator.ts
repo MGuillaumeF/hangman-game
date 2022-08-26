a0a2096af06f8dee7cc45ce13a50653dc71c70c7
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-08-26 23:36:44 +0200

    Create Validator.ts

---
+++ client/src/model/Validator.ts
@@ -0,0 +1,22 @@
+import { ModelError } from 
+
+export class Validator {
+
+public static checkStringProperty(constraint : {
+  mandatory : boolean;
+  min_length?: number;
+  max_length?: number;
+  pattern?: RegExp;
+}, value?: string) : ModelError[] {
+  return [];
+}
+
+public static checkStringProperty(constraint : {
+  mandatory : boolean;
+  min?: number;
+  max?: number;
+}, value?: number) : ModelError[] {
+  return [];
+}
+
+};
