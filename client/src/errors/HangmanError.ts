8ca272f5c9c5b34c462e2b5daa06b3f3ef899572
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-11-28 22:05:10 +0000

    [AUTO] prettier format write

--- client/src/errors/HangmanError.ts
+++ client/src/errors/HangmanError.ts
@@ -2,7 +2,7 @@ export default class HangmanError extends Error {
   // add specificity to debug
   public readonly name = "HangmanError";
   private static readonly date = Date.now();
-  constructor(message: string, options : any) {
+  constructor(message: string, options: any) {
     // call parent constructor
     super(message, options);
 
