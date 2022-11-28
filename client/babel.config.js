907d9c87aee7b179a876772b40d09c32b4538041
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-11-28 08:39:11 +0100

    Update babel.config.js

--- client/babel.config.js
+++ client/babel.config.js
@@ -18,7 +18,12 @@ module.exports = {
     ["@babel/plugin-proposal-private-methods", { loose: true }],
     "@babel/plugin-transform-react-constant-elements",
     "@babel/plugin-transform-runtime",
-    "@babel/plugin-transform-regenerator"
+    "@babel/plugin-transform-regenerator",
+    // add extends logic after conservation
+    // to have error parent link in error stack trace
+    ["babel-plugin-transform-builtin-extend", {
+            globals: ["Error", "Array"]
+        }]
   ],
   ignore: ["./dist", "./build", "./test", "./node_modules"]
 };
