417ec25bf65894ea795ae142427e28672c87edf3
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-11-28 08:35:43 +0100

    Create HangmanError.ts

---
+++ client/src/errors/HangmanError.ts
@@ -0,0 +1,14 @@
+export default class HangmanError extends Error {
+  // add specificity to debug
+  private static readonly name = 'HangmanError';
+  private static readonly date = Date.now();
+  constructor(message : string, options) {
+    // call parent constructor
+    super(message, options);
+
+    // error stack for V8
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, HangmanError);
+    }
+  }
+}
