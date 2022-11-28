e2c739df28a70b579e49a1695a634130a05476c8
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-11-29 00:15:56 +0100

    Update CustomError.spec.ts

--- client/test/ut/CustomError.spec.ts
+++ client/test/ut/CustomError.spec.ts
@@ -1,4 +1,4 @@
-const assert = require("assert");
+import assert from "assert"
 describe("Custom Error Test Suite", function () {
   it("should return -1 when the value is not present", function () {
     assert.equal([1, 2, 3].indexOf(4), -1);
