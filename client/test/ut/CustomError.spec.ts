4d016a264195358974b5792fc214797d55a2bba7
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-11-29 21:09:22 +0100

    Update CustomError.spec.ts (#1148)
    * Update CustomError.spec.ts
    
    * Update package.json
    
    * Update package.json
    
    * [AUTO] prettier format write
    
    * Create mocha-multi-config.json
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write

--- client/test/ut/CustomError.spec.ts
+++ client/test/ut/CustomError.spec.ts
@@ -1,6 +1,7 @@
 import assert from "assert";
 describe("Custom Error Test Suite", function () {
   it("should return -1 when the value is not present", function () {
+    // TODO remove sample test case
     assert.equal([1, 2, 3].indexOf(4), -1);
   });
 });
