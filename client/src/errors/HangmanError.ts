7178c7f8b07db32b9e1ec26ef34ce4501b954fe6
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-11-30 23:12:51 +0100

    Update CustomError.spec.ts (#1149)
    * Update CustomError.spec.ts
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write
    
    * Update CustomError.spec.ts
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write
    
    * Update package.json
    
    * Update .mocharc.jsonc
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write
    
    * Update CustomError.spec.ts
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write
    
    * Update HangmanError.ts
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write
    
    * Update CustomError.spec.ts
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write
    
    * Update CustomError.spec.ts
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write
    
    * Update CustomError.spec.ts
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write
    
    * Update CustomError.spec.ts
    
    * Update CustomError.spec.ts
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write
    
    * Update CustomError.spec.ts
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write
    
    * Update CustomError.spec.ts
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write
    
    * Update HangmanError.ts
    
    * Update CustomError.spec.ts
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write

--- client/src/errors/HangmanError.ts
+++ client/src/errors/HangmanError.ts
@@ -1,9 +1,11 @@
 export default class HangmanError extends Error {
   // add specificity to debug
   public readonly name = "HangmanError";
+  public cause: any;
   private static readonly date = Date.now();
-  constructor(message: string, options: any) {
+  constructor(message: string, options?: { cause: any }) {
     // call parent constructor
+    // @ts-ignore
     super(message, options);
 
     // error stack for V8
--- client/test/ut/CustomError.spec.ts
+++ client/test/ut/CustomError.spec.ts
@@ -1,7 +1,68 @@
-import assert from "assert";
+import { expect } from "chai";
+import HangmanError from "../../src/errors/HangmanError";
+
+function fakeParser(): void {
+  try {
+    JSON.parse("not a json string");
+  } catch (e) {
+    if (e instanceof Error) {
+      throw new HangmanError("Custom parsing error", { cause: e });
+    }
+  }
+}
+
 describe("Custom Error Test Suite", function () {
   it("should return -1 when the value is not present", function () {
-    // TODO remove sample test case
-    assert.equal([1, 2, 3].indexOf(4), -1);
+    try {
+      fakeParser();
+    } catch (e) {
+      console.error("error raised un catch", e);
+
+      expect(e instanceof HangmanError).to.equal(
+        true,
+        "Test error is hangman error instance"
+      );
+      if (e instanceof HangmanError) {
+        console.error("error message", e.message);
+        console.error("error stack", e.stack);
+        console.error("error string", e.toString());
+        expect(e.message).to.equal(
+          "Custom parsing error",
+          "test message of custom error"
+        );
+        expect(e?.stack).to.be.a(
+          "string",
+          "test stack attribute is present and is type string"
+        );
+        if (e?.stack) {
+          expect(e!.stack.includes("HangmanError")).to.equal(
+            true,
+            "test child error class name appear in stack trace"
+          );
+          expect(e!.stack.includes("CustomError.spec.ts:9")).to.equal(
+            true,
+            "test child rethrow error and custom error raised line appear in stack trace"
+          );
+          expect(e!.cause!.stack!.includes("CustomError.spec.ts:6")).to.equal(
+            true,
+            "test child rethrow error and original error raised line appear in stack trace"
+          );
+          expect(
+            e!.cause!.stack!.includes(
+              "SyntaxError: Unexpected token o in JSON at position 1"
+            )
+          ).to.equal(
+            true,
+            "test child rethrow error and original cause appear in stack trace"
+          );
+          expect(
+            e!.stack.includes("HangmanError: Custom parsing error")
+          ).to.equal(
+            true,
+            "test child rethrow error and custom cause appear in stack trace"
+          );
+        }
+      }
+    }
   });
 });
