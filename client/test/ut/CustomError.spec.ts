16d708082fcbf0e71fb9e34d648cef338694bfda
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-11-30 08:20:32 +0000

    [AUTO] prettier format write

--- client/test/ut/CustomError.spec.ts
+++ client/test/ut/CustomError.spec.ts
@@ -23,20 +23,41 @@ describe("Custom Error Test Suite", function () {
       console.error("error raised un catch", e);
       assert.equal(true, e instanceof HangmanError);
       if (e instanceof HangmanError) {
-        expect(e.message).to.equal("Custom parsing error", "test message of custom error");
-        expect(e?.stack).to.be.a("string", "test stack attribute is present and is type string");
+        expect(e.message).to.equal(
+          "Custom parsing error",
+          "test message of custom error"
+        );
+        expect(e?.stack).to.be.a(
+          "string",
+          "test stack attribute is present and is type string"
+        );
         if (e?.stack) {
-          expect(e!.stack.includes("HangmanError")).to.equal(true, "test child error class name appear in stack trace");
-          expect(e!.stack.includes("CustomError.spec.ts:9:13")).to.equal(true, "test child rethrow error and custom error raised line appear in stack trace");
-          expect(e!.stack.includes("CustomError.spec.ts:6:10")).to.equal(true, "test child rethrow error and original error raised line appear in stack trace");
+          expect(e!.stack.includes("HangmanError")).to.equal(
+            true,
+            "test child error class name appear in stack trace"
+          );
+          expect(e!.stack.includes("CustomError.spec.ts:9:13")).to.equal(
+            true,
+            "test child rethrow error and custom error raised line appear in stack trace"
+          );
+          expect(e!.stack.includes("CustomError.spec.ts:6:10")).to.equal(
+            true,
+            "test child rethrow error and original error raised line appear in stack trace"
+          );
           expect(
             e!.stack.includes(
               "SyntaxError: Unexpected token o in JSON at position 1"
             )
-          ).to.equal(true, "test child rethrow error and original cause appear in stack trace");
+          ).to.equal(
+            true,
+            "test child rethrow error and original cause appear in stack trace"
+          );
           expect(
             e!.stack.includes("HangmanError: Custom parsing error")
-          ).to.equal(true, "test child rethrow error and custom cause appear in stack trace");
+          ).to.equal(
+            true,
+            "test child rethrow error and custom cause appear in stack trace"
+          );
         }
       }
     }
