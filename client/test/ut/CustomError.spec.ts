d9fa39463e946dfc792844685dd9e629e08d3a20
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-11-30 09:01:43 +0100

    Update CustomError.spec.ts

--- client/test/ut/CustomError.spec.ts
+++ client/test/ut/CustomError.spec.ts
@@ -23,22 +23,20 @@ describe("Custom Error Test Suite", function () {
       console.error("error raised un catch", e);
       assert.equal(true, e instanceof HangmanError);
       if (e instanceof HangmanError) {
-        expect(e.message).to.equal("Custom parsing error");
-        expect(e?.stack).to.be.a("string");
+        expect(e.message).to.equal("Custom parsing error", "test message of custom error");
+        expect(e?.stack).to.be.a("string", "test stack attribute is present and is type string");
         if (e?.stack) {
-          // test child error class name appear in stack trace
-          expect(e!.stack.includes("HangmanError")).to.equal(true);
-          // test child rethrow line appear in stack trace
-          expect(e!.stack.includes("CustomError.spec.ts:9:13")).to.equal(true);
-          expect(e!.stack.includes("CustomError.spec.ts:6:10")).to.equal(true);
+          expect(e!.stack.includes("HangmanError")).to.equal(true, "test child error class name appear in stack trace");
+          expect(e!.stack.includes("CustomError.spec.ts:9:13")).to.equal(true, "test child rethrow error and custom error raised line appear in stack trace");
+          expect(e!.stack.includes("CustomError.spec.ts:6:10")).to.equal(true, "test child rethrow error and original error raised line appear in stack trace");
           expect(
             e!.stack.includes(
               "SyntaxError: Unexpected token o in JSON at position 1"
             )
-          ).to.equal(true);
+          ).to.equal(true, "test child rethrow error and original cause appear in stack trace");
           expect(
             e!.stack.includes("HangmanError: Custom parsing error")
-          ).to.equal(true);
+          ).to.equal(true, "test child rethrow error and custom cause appear in stack trace");
         }
       }
     }
