423424b74d792da96311596c87b259066303f0d7
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-12-02 19:19:01 +0000

    [AUTO] prettier format write

--- client/test/ut/model/Word.spec.ts
+++ client/test/ut/model/Word.spec.ts
@@ -3,27 +3,25 @@ import Word from "../../src/model/Word";
 
 describe("Word Model Object Test Suite", function () {
   it("Test parse method", function () {
-   
-      const myWord = Word.parse({
-        name : "hello",
-        definitions : ["salutation word"]
-      });
-      expect(myWord instanceof Word).to.equal(
-        true,
-        "Test word parser object is word instance"
+    const myWord = Word.parse({
+      name: "hello",
+      definitions: ["salutation word"]
+    });
+    expect(myWord instanceof Word).to.equal(
+      true,
+      "Test word parser object is word instance"
+    );
+    if (myWord instanceof Word) {
+      console.info("word parsed", myWord);
+
+      expect(myWord.name).to.be.a(
+        "string",
+        "test stack attribute is present and is type string"
       );
-      if (myWord instanceof Word) {
-        console.info("word parsed", myWord);
-        
-        expect(myWord.name).to.be.a(
-          "string",
-          "test stack attribute is present and is type string"
-        );
-        expect(myWord.definitions).to.be.a(
-          "array",
-          "test definition attribute is present and is type array"
-        );
-        
-      }
+      expect(myWord.definitions).to.be.a(
+        "array",
+        "test definition attribute is present and is type array"
+      );
+    }
   });
 });
