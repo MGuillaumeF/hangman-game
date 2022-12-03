1d06083a95b70ef5565126de6778c9fef15fc8a3
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-12-03 00:25:11 +0000

    [AUTO] prettier format write

--- client/test/ut/model/Word.spec.ts
+++ client/test/ut/model/Word.spec.ts
@@ -34,27 +34,27 @@ describe("Word Model Object Test Suite", function () {
   });
 
   it("Test toJson method", function () {
-    const myWord : any = Word.parse({
+    const myWord: any = Word.parse({
       name: "hello",
       definitions: ["salutation word"]
     }).toJson();
-      console.info("word json object", myWord);
+    console.info("word json object", myWord);
 
-      expect(myWord.name).to.be.a(
-        "string",
-        "test name attribute is present and is type string"
-      );
-      expect(myWord.definitions).to.be.a(
-        "array",
-        "test definitions attribute is present and is type array"
-      );
-      expect(myWord.name).to.equal(
-        "hello",
-        "test name attribute is present and has good value"
-      );
-      expect(myWord.definitions[0]).to.equal(
-        "salutation word",
-        "test definitions attribute is present and has good value in first case"
-      );
+    expect(myWord.name).to.be.a(
+      "string",
+      "test name attribute is present and is type string"
+    );
+    expect(myWord.definitions).to.be.a(
+      "array",
+      "test definitions attribute is present and is type array"
+    );
+    expect(myWord.name).to.equal(
+      "hello",
+      "test name attribute is present and has good value"
+    );
+    expect(myWord.definitions[0]).to.equal(
+      "salutation word",
+      "test definitions attribute is present and has good value in first case"
+    );
   });
 });
