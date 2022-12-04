245c8d29d56d7d739b13ddbb0cf3f1355fabac2d
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-12-04 18:22:27 +0100

    Update HangmanError.ts

--- client/src/errors/HangmanError.ts
+++ client/src/errors/HangmanError.ts
@@ -2,9 +2,22 @@
  * Custom application error
  */
 export default class HangmanError extends Error {
-  // add specificity to debug
+  /** 
+   * error name to get it when errro is raised
+   */
+  public readonly name = HangmanError.name;
+  /**
+   * error cause if error is a rethrow
+   */
   public cause?: Error;
+  /**
+   * the date of error throw
+   */
   private static readonly date = Date.now();
+  /**
+   * @param message the message of error
+   * @param options the cause or error
+   */
   constructor(message: string, options?: { cause: Error }) {
     // call parent constructor
     // @ts-ignore
