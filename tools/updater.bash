656dd7bca1c41ab93292ff296c9a31fe6210e2ff
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2022-12-24 03:03:04 +0100

    Update updater.bash

--- tools/updater.bash
+++ tools/updater.bash
@@ -4,13 +4,23 @@ dependency_client_update=$(<client/dependency_client_update.log)
 dependency_model_generator_update=$(<data-management/tools/model-generator/dependency_model_generator_update.log)
 
 if [[ `git status --porcelain` ]]; then
-  git stash
-  git checkout -b feature/full-npm-updates
-  git push -u origin feature/full-npm-updates
+  echo "=> Set configuration"
+  git push origin/feature/full-npm-updates --delete feature/full-npm-updates || echo "clean branches failed"
   git config user.name "MGuillaumeF"
   git config user.email "$EMAIL"
+  echo "=> List of Updates :"
+  git status
+  git stash
+  echo "=> Create Branch"
+  git checkout -b feature/full-npm-updates
+  git push -u origin feature/full-npm-updates
+  echo "=> Index files"
+  git stash pop
   git add client || echo "No modification to add pop"
   git add data-management/tools || echo "No modification to add pop"
+  echo "=> Indexed Changes"
+  git status
+  echo "=> Save Changes"
   git commit -m "[AUTO][NPM_UPDATE] npm check updates
 
 client : $dependency_client_update
