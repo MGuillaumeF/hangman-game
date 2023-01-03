0603223eefd8ee2262810e0adfea8b363844b429
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2023-01-03 08:29:21 +0100

    Create first-compo-test.cy.ts

---
+++ client/cypress/components/first-compo-test.cy.ts
@@ -0,0 +1,6 @@
+import Button from '../../src/components/BasicButton/Button'
+
+it('uses custom text for the button label', () => {
+  cy.mount(<Button id="sample" type="button">Click me!</Button>)
+  cy.get('button').should('contains.text', 'Click me!')
+})
