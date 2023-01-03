7846f06f738349ebcc8a944ec18b389c2c0490e9
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2023-01-04 00:57:04 +0100

    Update first-compo-test.cy.ts

--- client/cypress/component/first-compo-test.cy.ts
+++ client/cypress/component/first-compo-test.cy.ts
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '../../src/components/BasicButton/Button'
 
 it('uses custom text for the button label', () => {
-  cy.mount(<Button id="sample" type="button">Click me!</Button>)
+  const compo = <><Button id="sample" type="button">Click me!</Button></>
+  cy.mount(compo)
   cy.get('button').should('contains.text', 'Click me!')
 })
