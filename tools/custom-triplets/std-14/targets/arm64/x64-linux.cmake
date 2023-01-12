441e0454984953deaac05b588a7e70edc50df5d5
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2023-01-12 03:00:40 +0100

    Update x64-linux.cmake

--- tools/custom-triplets/std-14/targets/arm64/x64-linux.cmake
+++ tools/custom-triplets/std-14/targets/arm64/x64-linux.cmake
@@ -1,7 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_SOURCE_DIR}/../../../../toolchains/Toolchain-RaspberryPi.cmake")
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../../../../toolchains/Toolchain-RaspberryPi.cmake")
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
