ba9df45c959bf71686eb86626bbdb225a5f61e48
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2023-01-23 02:42:16 +0100

    Mise en place du composant du pendu avec couleur

--- client/src/components/Hangman/Hangman.tsx
+++ client/src/components/Hangman/Hangman.tsx
@@ -1,95 +1,169 @@
 import React from "react";
 
+/**
+ * The min value to display hangman draw
+ */
+export const MIN_HANGMAN_VALUE = 0;
+/**
+ * The max value to display hangman draw
+ */
+export const MAX_HANGMAN_VALUE = 11;
+
 type Props = {
+  /**
+   * The background color of drawing area
+   */
+  backgroundColor?: string;
+  /**
+   * The stroke color of parts of hangman draw
+   */
+  color?: string;
+  /**
+   * The height of Hangman draw
+   */
   height: number;
+  /**
+   * The quantity of Hangman's parts to draw
+   */
   quantity: number;
+  /**
+   * The width of Hangman draw
+   */
   width: number;
 };
 
+/**
+ * SVG Path Component Builder Wrapper
+ * @param param0 @see SVGPathElement
+ * @returns JSX.Element of SVG draw
+ */
 function Path({ children, ...props }: React.SVGProps<SVGPathElement>) {
   return <path {...props}>{children}</path>;
 }
+/**
+ * SVG Circle Component Builder Wrapper
+ * @param param0 @see SVGCircleElement
+ * @returns JSX.Element of SVG draw
+ */
 function Circle({ children, ...props }: React.SVGProps<SVGCircleElement>) {
   return <circle {...props}>{children}</circle>;
 }
 
 interface IPartProps extends React.SVGProps<SVGElement> {
-  Type: Function;
+  /**
+   * SVG Component Builder Wrapper
+   * @param props The props to draw the part of hangman
+   * @returns The JSX.Element of the part of hangman
+   */
+  Type: (props: React.SVGProps<any>) => JSX.Element;
 }
 
 const parts: IPartProps[] = [
   {
+    /**
+     * Bottom support
+     */
     Type: Path,
     d: "M0 487.5 L125 487.5",
-    stroke: "#000000",
     strokeWidth: "25"
   },
   {
+    /**
+     * Column support
+     */
     Type: Path,
     d: "M37.5 475 L37.5 10",
-    stroke: "#000000",
     strokeWidth: "25"
   },
   {
+    /**
+     * Top support
+     */
     Type: Path,
     d: "M37.5 22.5 L245 22.5",
-    stroke: "#000000",
     strokeWidth: "25"
   },
   {
+    /**
+     * Force Leg Bar Join
+     */
     Type: Path,
     d: "M40 97.5 L122.5 25",
-    stroke: "#000000",
     strokeWidth: "18"
   },
   {
+    /**
+     * Rope
+     */
     Type: Path,
     d: "M247.5 10 L247.5 75",
-    stroke: "#000000",
     strokeWidth: "6"
   },
   {
+    /**
+     * The Head
+     */
     Type: Circle,
     cx: "247.5",
     cy: "125",
     r: "50",
     fill: "transparent",
-    stroke: "#000000",
     strokeWidth: "4"
   },
   {
+    /**
+     * Body
+     */
     Type: Path,
     d: "M247.5 175 L247.5 300",
-    stroke: "#000000",
     strokeWidth: "4"
   },
   {
-    Type: Path,
-    d: "M247.5 300 L300.5 335",
-    stroke: "#000000",
-    strokeWidth: "4"
-  },
-  {
-    Type: Path,
-    d: "M247.5 300 L194.5 335",
-    stroke: "#000000",
-    strokeWidth: "4"
-  },
-  {
-    Type: Path,
-    d: "M247.5 220 L300.5 255",
-    stroke: "#000000",
-    strokeWidth: "4"
-  },
-  {
+    /**
+     * Arm
+     */
     Type: Path,
     d: "M247.5 220 L194.5 255",
-    stroke: "#000000",
+    strokeWidth: "4"
+  },
+  {
+    /**
+     * Arm
+     */
+    Type: Path,
+    d: "M247.5 220 L300.5 255",
+    strokeWidth: "4"
+  },
+  {
+    /**
+     * leg
+     */
+    Type: Path,
+    d: "M247.5 300 L300.5 335",
+    strokeWidth: "4"
+  },
+  {
+    /**
+     * leg
+     */
+    Type: Path,
+    d: "M247.5 300 L194.5 335",
     strokeWidth: "4"
   }
 ];
 
-function Hangman({ width, height, quantity }: Props) {
+/**
+ * Component to draw Hangman SVG
+ * @param param0 @see Props
+ * @returns The Hangman Draw Component
+ */
+function Hangman({
+  width,
+  height,
+  quantity,
+  color,
+  backgroundColor
+}: Props): JSX.Element {
   return (
     <svg
       version="1.1"
@@ -98,37 +172,35 @@ function Hangman({ width, height, quantity }: Props) {
       height={String(height)}
       xmlns="http://www.w3.org/2000/svg"
     >
+      <rect
+        fill={backgroundColor || "transparent"}
+        width={String(width)}
+        height={String(height)}
+      />
       {parts.slice(0, quantity).map((value: IPartProps, index: number) => {
         const { Type, ...props } = value;
         if (index === quantity - 1) {
           return (
-            <Type key={index} {...props} strokeDasharray="2000">
+            <Type
+              key={index}
+              {...props}
+              stroke={color || "#000000"}
+              strokeDasharray="2000"
+            >
               <animate
-                attributeName="strokeDashoffset"
-                values="2037;0;2037"
-                begin="2s"
-                dur="5"
                 calcMode="spline"
+                attributeName="stroke-dashoffset"
+                from="2000"
+                to="0"
+                begin="0s"
+                dur="4s"
               />
             </Type>
           );
         } else {
-          return <Type key={index} {...props} />;
+          return <Type key={index} {...props} stroke={color || "#000000"} />;
         }
       })}
-      <path
-        d="M0 487.5 L125 487.5"
-        stroke="#000000"
-        strokeWidth="25"
-        strokeDasharray="2000"
-      >
-        <animate
-          attributeName="strokeDashoffset"
-          values="2037;0;2037"
-          dur="5"
-          calcMode="spline"
-        />
-      </path>
     </svg>
   );
 }
--- client/src/components/pages/Home/Home.tsx
+++ client/src/components/pages/Home/Home.tsx
@@ -1,15 +1,37 @@
-import React from "react";
-import Hangman from "../../Hangman/Hangman";
+import React, { useState } from "react";
+import Hangman, {
+  MAX_HANGMAN_VALUE,
+  MIN_HANGMAN_VALUE
+} from "../../Hangman/Hangman";
 
 type Props = {
   id: string;
 };
 
+const onChange =
+  (updater: React.Dispatch<React.SetStateAction<number>>) =>
+  (e: React.ChangeEvent<HTMLInputElement>) =>
+    updater(Number(e.target.value));
+
 function Home({ id }: Props) {
+  const [errorNumber, setErrorNumber] = useState(0);
+
   return (
     <div id={id}>
       <p>Home</p>
-      <Hangman width={500} height={500} quantity={11} />
+
+      <input
+        type="number"
+        min={MIN_HANGMAN_VALUE}
+        max={MAX_HANGMAN_VALUE}
+        onChange={onChange(setErrorNumber)}
+      />
+      <Hangman
+        width={500}
+        height={500}
+        quantity={errorNumber}
+        color="#555555"
+      />
     </div>
   );
 }
