48d833e073060a23aef4d80a9a33c617ab3ccd51
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2023-05-22 18:35:42 +0200

    Update RPI dependencies (#1390)
    * Update RPI dependencies
    
    * [AUTO] prettier format write
    
    * Update default.yml
    
    * Update default.yml
    
    * [AUTO] clang format write
    
    * Update default.yml
    
    * [AUTO] prettier format write
    
    * Update Toolchain-RaspberryPi.cmake
    
    * [AUTO] clang format write
    
    * [AUTO] prettier format write

--- tools/toolchains/Toolchain-RaspberryPi.cmake
+++ tools/toolchains/Toolchain-RaspberryPi.cmake
@@ -5,8 +5,8 @@ set(CMAKE_SYSROOT /home/devel/rasp-pi-rootfs)
 set(CMAKE_STAGING_PREFIX /home/devel/stage)
 
 set(tools /usr)
-set(CMAKE_C_COMPILER ${tools}/bin/aarch64-linux-gnu-gcc-9)
-set(CMAKE_CXX_COMPILER ${tools}/bin/aarch64-linux-gnu-g++-9)
+set(CMAKE_C_COMPILER ${tools}/bin/aarch64-linux-gnu-gcc-12)
+set(CMAKE_CXX_COMPILER ${tools}/bin/aarch64-linux-gnu-g++-12)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
