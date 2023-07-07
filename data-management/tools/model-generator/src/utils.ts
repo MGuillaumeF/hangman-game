4a15cd26482bad8ee45a67cba7756ba6b18da3cf
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2023-07-07 03:10:42 +0200

    Ajout des traces GCC dans sonar

--- data-management/tools/model-generator/src/utils.ts
+++ data-management/tools/model-generator/src/utils.ts
@@ -1,16 +1,16 @@
 /**
- *
- * @param str
- * @returns
+ * To convert lower case first char to upercase
+ * @param str The string to convert
+ * @returns The string with capitalise first char
  */
 export function toCapitalize(str: string): string {
   return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
 }
 
 /**
- *
- * @param str
- * @returns
+ * To convert snake_case string to camelCase
+ * @param str The string to convert
+ * @returns The string cCamelCase
  */
 export function snakeCaseToCamelCase(str: string): string {
   const words = str.split("_");
@@ -18,9 +18,9 @@ export function snakeCaseToCamelCase(str: string): string {
 }
 
 /**
- *
- * @param str
- * @returns
+ * To convert snake_case string to UperCamelCase
+ * @param str The string to convert
+ * @returns The string UperCamelCase
  */
 export function snakeCaseToUpperCamelCase(str: string): string {
   const words = str.split("_");
